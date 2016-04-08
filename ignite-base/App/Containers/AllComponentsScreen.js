4ad50c3500c86ac3145249af60ad6ab485e68467
Mark Rickert <mjar81@gmail.com>
2016-04-08 10:11:36 -0400

    Update to meet linting standards.

--- ignite-base/App/Containers/AllComponentsScreen.js
+++ ignite-base/App/Containers/AllComponentsScreen.js
@@ -40,7 +40,7 @@ export default class AllComponentsScreen extends React.Component {
   componentWillReceiveProps (nextProps) {
     // Request premissions only if the user has logged in.
     const { loggedIn } = nextProps
-    if ( loggedIn ) {
+    if (loggedIn) {
       console.log('Requesting push notification permissions.')
       PushNotification.requestPermissions()
     }
--- ignite-base/App/Root.js
+++ ignite-base/App/Root.js
@@ -14,38 +14,37 @@ const store = configureStore()
 // https://github.com/zo0r/react-native-push-notification
 PushNotification.configure({
 
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function(token) {
-        console.log( 'TOKEN:', token );
-    },
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: (token) => {
+    console.log('TOKEN:', token)
+  },
 
-    // (required) Called when a remote or local notification is opened or received
-    onNotification: function(notification) {
-        console.log( 'NOTIFICATION:', notification );
-    },
+  // (required) Called when a remote or local notification is opened or received
+  onNotification: (notification) => {
+    console.log('NOTIFICATION:', notification)
+  },
 
-    // ANDROID ONLY: (optional) GCM Sender ID.
-    senderID: "YOUR GCM SENDER ID",
+  // ANDROID ONLY: (optional) GCM Sender ID.
+  senderID: 'YOUR GCM SENDER ID',
 
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-        alert: true,
-        badge: true,
-        sound: true
-    },
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true
+  },
 
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
 
-    /**
-      * IOS ONLY: (optional) default: true
-      * - Specified if permissions will requested or not,
-      * - if not, you must call PushNotificationsHandler.requestPermissions() later
-      */
-    requestPermissions: false,
-});
-
+  /**
+    * IOS ONLY: (optional) default: true
+    * - Specified if permissions will requested or not,
+    * - if not, you must call PushNotificationsHandler.requestPermissions() later
+    */
+  requestPermissions: false
+})
 
 export default class RNBase extends React.Component {
 
