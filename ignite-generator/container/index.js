f666c07ce4c05c5517c098faaa10386a9a1060b4
Gant <GantMan@gmail.com>
2016-04-08 13:11:42 -0500

    removed unused import

--- ignite-generator/container/index.js
+++ ignite-generator/container/index.js
@@ -4,15 +4,9 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _safe = require('colors/safe');
-
-var _safe2 = _interopRequireDefault(_safe);
-
 var _yeomanGenerator = require('yeoman-generator');
 
 var _shared = require('../shared/shared');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
