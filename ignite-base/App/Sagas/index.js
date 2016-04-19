a8236f349b11b9ec723c581c52289cf4fe36a832
Steve Kellock <steve@kellock.ca>
2016-04-18 21:08:58 -0400

    Upgraded HttpClient to Apisauce.

--- ignite-base/App/Sagas/index.js
+++ ignite-base/App/Sagas/index.js
@@ -1,10 +1,16 @@
+import API from '../Services/Api'
 import { watchStartup } from './StartupSaga'
 import { watchLoginAttempt } from './LoginSaga'
-import { watchWeatherRequest } from './WeatherSaga'
+import getCityWeather from './GetCityWeatherSaga'
+
+// Create our API at this level and feed it into
+// the sagas that are expected to make API calls
+// so there's only 1 copy app-wide!
+const api = API.create()
 
 // start the daemons
 export default [
   watchStartup,
   watchLoginAttempt,
-  watchWeatherRequest
+  getCityWeather(api).watcher
 ]
