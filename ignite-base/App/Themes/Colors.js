0619057911395e62ac215e859e7546c53b4d9e4c
Gant <GantMan@gmail.com>
2016-05-29 13:53:27 -0500

    removed 5 useless colors

--- ignite-base/App/Themes/Colors.js
+++ ignite-base/App/Themes/Colors.js
@@ -1,9 +1,6 @@
 const colors = {
   background: '#1F0808',
   clear: 'rgba(0,0,0,0)',
-  ocean: '#415572',
-  deepOcean: '#32435c',
-  wave: '#0083b0',
   facebook: '#3b5998',
   transparent: 'rgba(0,0,0,0)',
   silver: '#F7F7F7',
@@ -11,13 +8,11 @@ const colors = {
   error: 'rgba(200, 0, 0, 0.8)',
   ricePaper: 'rgba(255,255,255, 0.75)',
   cloud: 'rgba(200,200,200, 0.35)',
-  smoke: 'rgba(100, 100, 100, 0.9)',
   windowTint: 'rgba(0, 0, 0, 0.4)',
   panther: '#161616',
   charcoal: '#595959',
   coal: '#2d2d2d',
   bloodOrange: '#fb5f26',
-  bloodOrangeJuice: 'rgba(251, 95, 38, 0.8)',
   snow: 'white',
   ember: 'rgba(164, 0, 48, 0.5)',
   fire: '#e73536'
