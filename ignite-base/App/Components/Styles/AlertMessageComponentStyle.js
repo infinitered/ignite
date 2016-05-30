0b9e16a7182441017f426d915366e61c3543f055
fvonhoven <frank@infinite.red>
2016-05-30 14:56:08 -0500

    AlertMessage added, added to AllComponentsScreen, & I18n

---
+++ ignite-base/App/Components/Styles/AlertMessageComponentStyle.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Metrics, Fonts } from '../../Themes/'
+
+export default StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginVertical: Metrics.section
+  },
+  contentContainer: {
+    alignSelf: 'center',
+    alignItems: 'center'
+  },
+  message: {
+    marginTop: Metrics.baseMargin,
+    marginHorizontal: Metrics.baseMargin,
+    textAlign: 'center',
+    fontFamily: Fonts.base,
+    fontSize: Fonts.size.regular,
+    fontWeight: 'bold',
+    color: Colors.steel
+  },
+  icon: {
+    color: Colors.steel
+  }
+})
