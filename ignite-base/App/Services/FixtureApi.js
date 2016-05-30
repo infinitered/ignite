e5fe5717bec6bdc99a8c4abf5cc57f34e243dabf
fvonhoven <frank@infinite.red>
2016-05-30 16:50:16 -0500

    Add 2 city Fixtures, FixtureAPI, & cond in index

---
+++ ignite-base/App/Services/FixtureApi.js
@@ -0,0 +1,12 @@
+export default {
+  // Functions return fixtures
+  getCity: (city) => {
+    // This fixture only supports Boise or else returns toronto
+    const boiseData = require('../Fixtures/boise.json')
+    const torontoData = require('../Fixtures/toronto.json')
+    return {
+      ok: true,
+      data: city.toLowerCase() === 'boise' ? boiseData : torontoData
+    }
+  }
+}
