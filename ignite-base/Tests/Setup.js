053c04f1e0ab4d6e8557902c0fb437a6b5b515a6
Steve Kellock <steve@kellock.ca>
2016-06-21 10:35:34 -0400

    Adds React Native component tests.

--- ignite-base/Tests/Setup.js
+++ ignite-base/Tests/Setup.js
@@ -1,7 +1,20 @@
-// import test from 'ava'
-// import chai from 'chai'
-// import chaiImmutable from 'chai-immutable'
+import mockery from 'mockery'
 
-// chai.use(chaiImmutable)
-// global.expect = chai.expect
-// global.__DEV__ = true
+// inject __DEV__ as it is not available when running through the tests
+global.__DEV__ = true
+
+// We enable mockery and leave it on.  This is a little different than what
+// I've seen setup.
+mockery.enable()
+
+// Silence the warnings when *real* modules load... this is a change from
+// the norm.  We want to opt-in instead of opt-out because not everything
+// will be mocked.
+mockery.warnOnUnregistered(false)
+
+// mock the images so the tests can load them
+mockery.registerMock('../Images/ir.png', 0)
+mockery.registerMock('../Images/top_logo.png', 0)
+mockery.registerMock('../Images/ignite_logo.png', 0)
+mockery.registerMock('../Images/tile_bg.png', 0)
+mockery.registerMock('../Images/BG.png', 0)
