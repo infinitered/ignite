5fa0ff978c747b470b09ecb223fb9ac6b6437260
Gant <GantMan@gmail.com>
2016-07-17 22:23:33 -0500

    updated firedrill to enforce locks + refactor

--- fireTest.sh
+++
@@ -1,7 +0,0 @@
-# ignite generator version
-node -e "var o = require('./ignite-generator/package.json'); console.log(o.version);"
-
-# See if version is in index
-grep -q '1.3.2' ignite-generator/src/app/index.es
-
-# see if version is matched in CLI
