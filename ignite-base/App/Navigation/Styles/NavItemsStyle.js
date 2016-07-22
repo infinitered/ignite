7cda773869b79856cb9761cf72a206a7b5f85281
Gant <GantMan@gmail.com>
2016-07-22 15:32:00 -0500

    start nav items list with some friendly buttons

---
+++ ignite-base/App/Navigation/Styles/NavItemsStyle.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+import { Metrics, Colors } from '../../Themes/'
+
+export default StyleSheet.create({
+  navButtonLeft: {
+    marginTop: Metrics.doubleBaseMargin,
+    marginLeft: Metrics.baseMargin,
+    backgroundColor: Colors.transparent,
+    width: Metrics.icons.medium
+  }
+})
