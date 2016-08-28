6f1a8fe5aeb98bb6c1bf222970ce3511efa5889d
Gant Laborde <GantMan@gmail.com>
2016-08-28 16:16:03 -0500

    we're not really supporting exponent anymore

--- ignite-base/index.js
+++
@@ -1,8 +0,0 @@
-// This file is here if you'd like to use Exponent XDE
-// Swap out the RN dependency for the latest from Exponent.
-// E.G. "react-native": "github:exponentjs/react-native#2016-01-02"
-
-import { AppRegistry } from 'react-native'
-import Root from './App/Root'
-
-AppRegistry.registerComponent('main', () => Root)
