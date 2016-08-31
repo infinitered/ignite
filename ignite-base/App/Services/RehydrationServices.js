2126f045099d0f6cd3b695b9cdca8226bfd00bed
fvonhoven <frank@infinite.red>
2016-08-31 10:27:00 -0500

    Ad cool & clean friendly logging for Reactotron output

--- ignite-base/App/Services/RehydrationServices.js
+++ ignite-base/App/Services/RehydrationServices.js
@@ -1,16 +1,23 @@
 import ReduxPersist from '../Config/ReduxPersist'
 import { AsyncStorage } from 'react-native'
 import { persistStore } from 'redux-persist'
-import Reactotron from 'reactotron-react-native'
 
 const updateReducers = (store) => {
   const reducerVersion = ReduxPersist.reducerVersion
   const config = ReduxPersist.storeConfig
 
+
   // Check to ensure latest reducer version
   AsyncStorage.getItem('reducerVersion').then((localVersion) => {
     if (localVersion !== reducerVersion) {
-      Reactotron.log(`PURGING STORE ${localVersion} 'vs.' ${reducerVersion}`)
+      console.tron.display({
+        name: 'PURGE',
+        value: `Reducer Version Change
+        Old Version: ${localVersion}
+        New Version: ${reducerVersion}`,
+        preview: 'Purging Store',
+        important: true
+      })
       // Purge store
       persistStore(store, config).purge()
       AsyncStorage.setItem('reducerVersion', reducerVersion)
