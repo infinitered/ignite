a60f01bfd3b6d8df227a245c5ff93bd9a56c0e90
Steve Kellock <steve@kellock.ca>
2016-09-08 15:16:15 -0400

    Fixes my broken test.

--- ignite-base/Tests/Sagas/StartupSagaTest.js
+++ ignite-base/Tests/Sagas/StartupSagaTest.js
@@ -7,7 +7,7 @@ const stepper = (fn) => (mock) => fn.next(mock).value
 
 test('watches for the right action', t => {
   const step = stepper(startup())
-  TemperatureActions.requestTemperature('San Francisco')
+  TemperatureActions.temperatureRequest('San Francisco')
   t.deepEqual(step(), select(selectTemperature))
-  t.deepEqual(step(), put(TemperatureActions.requestTemperature('San Francisco')))
+  t.deepEqual(step(), put(TemperatureActions.temperatureRequest('San Francisco')))
 })
