73bcc0044d0a2340f414abffb998f5d86e9c3e7c
Gant <GantMan@gmail.com>
2016-09-17 23:53:03 -0500

    visual spiff ups :eyes:

--- ignite-base/Tests/Sagas/TemperatureSagaTest.js
+++ ignite-base/Tests/Sagas/TemperatureSagaTest.js
@@ -6,13 +6,6 @@ import TemperatureActions from '../../App/Redux/TemperatureRedux'
 
 const stepper = (fn) => (mock) => fn.next(mock).value
 
-// test('watches for the right action', t => {
-//   const step = stepper(startup())
-//   TemperatureActions.temperatureRequest('San Francisco')
-//   t.deepEqual(step(), select(selectTemperature))
-//   t.deepEqual(step(), put(TemperatureActions.temperatureRequest('San Francisco')))
-// })
-
 test('first calls API', t => {
   const step = stepper(getTemperature(FixtureAPI, {city: 'taco'}))
   // first yield is API
