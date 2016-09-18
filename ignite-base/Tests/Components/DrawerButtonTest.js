43119d0fc7dcc1e6872c74be67ed8e13a5ccc22c
Gant <GantMan@gmail.com>
2016-09-17 22:24:33 -0500

    updated and added tests

--- ignite-base/Tests/Components/DrawerButtonTest.js
+++ ignite-base/Tests/Components/DrawerButtonTest.js
@@ -1,29 +1,29 @@
 // https://github.com/airbnb/enzyme/blob/master/docs/api/shallow.md
 import test from 'ava'
 import React from 'react'
-import FullButton from '../../App/Components/FullButton'
+import DrawerButton from '../../App/Components/DrawerButton'
 import { shallow } from 'enzyme'
 
-// Basic wrapper
-const wrapper = shallow(<FullButton onPress={() => {}} text='hi' />)
-
 test('component exists', t => {
+  const wrapper = shallow(<DrawerButton onPress={() => {}} text='hi' />)
   t.is(wrapper.length, 1) // exists
 })
 
 test('component structure', t => {
+  const wrapper = shallow(<DrawerButton onPress={() => {}} text='hi' />)
+
   t.is(wrapper.name(), 'TouchableOpacity') // the right root component
   t.is(wrapper.children().length, 1) // has 1 child
   t.is(wrapper.children().first().name(), 'Text') // that child is Text
 })
 
 test('onPress', t => {
-  let i = 0 // i guess i could have used sinon here too... less is more i guess
+  let i = 0
   const onPress = () => i++
-  const wrapperPress = shallow(<FullButton onPress={onPress} text='hi' />)
+  const wrapper = shallow(<DrawerButton onPress={onPress} text='hi' />)
 
-  t.is(wrapperPress.prop('onPress'), onPress) // uses the right handler
+  t.is(wrapper.prop('onPress'), onPress) // uses the right handler
   t.is(i, 0)
-  wrapperPress.simulate('press')
+  wrapper.simulate('press')
   t.is(i, 1)
 })
--- ignite-base/Tests/Components/RoundedButtonTest.js
+++ ignite-base/Tests/Components/RoundedButtonTest.js
@@ -4,32 +4,36 @@ import React from 'react'
 import RoundedButton from '../../App/Components/RoundedButton'
 import { shallow } from 'enzyme'
 
+// Basic wrapper
+const wrapper = shallow(<RoundedButton onPress={() => {}} text='howdy' />)
+
 test('component exists', t => {
-  const wrapper = shallow(<RoundedButton onPress={() => {}} text='hi' />)
   t.is(wrapper.length, 1) // exists
 })
 
 test('component structure', t => {
-  const wrapper = shallow(<RoundedButton onPress={() => {}} text='hi' />)
-
   t.is(wrapper.name(), 'TouchableOpacity') // the right root component
   t.is(wrapper.children().length, 1) // has 1 child
   t.is(wrapper.children().first().name(), 'Text') // that child is Text
 })
 
+test('the text is set properly - uppercase', t => {
+  t.is(wrapper.children().first().props().children, 'HOWDY')
+})
+
 test('onPress', t => {
   let i = 0 // i guess i could have used sinon here too... less is more i guess
   const onPress = () => i++
-  const wrapper = shallow(<RoundedButton onPress={onPress} text='hi' />)
+  const wrapperPress = shallow(<RoundedButton onPress={onPress} text='hi' />)
 
-  t.is(wrapper.prop('onPress'), onPress) // uses the right handler
+  t.is(wrapperPress.prop('onPress'), onPress) // uses the right handler
   t.is(i, 0)
-  wrapper.simulate('press')
+  wrapperPress.simulate('press')
   t.is(i, 1)
 })
 
 test('renders children text when passed', t => {
-  const wrapper = shallow(<RoundedButton onPress={() => {}}>Howdy</RoundedButton>)
-  t.is(wrapper.children().length, 1) // has 1 child
-  t.is(wrapper.children().first().name(), 'Text') // that child is Text
+  const wrapperChild = shallow(<RoundedButton onPress={() => {}}>Howdy</RoundedButton>)
+  t.is(wrapperChild.children().length, 1) // has 1 child
+  t.is(wrapperChild.children().first().name(), 'Text') // that child is Text
 })
