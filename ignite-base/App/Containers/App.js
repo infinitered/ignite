1123ce42739c4344618aa75174dd8bccb8dd796b
Gant Laborde <GantMan@gmail.com>
2016-09-27 11:08:29 -0500

    fixed possible i18n bug

--- ignite-base/App/Containers/App.js
+++ ignite-base/App/Containers/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-
 import { Provider } from 'react-redux'
+import '../I18n/I18n' // keep before root container
 import RootContainer from './RootContainer'
 import createStore from '../Redux'
 import applyConfigSettings from '../Config'
-import '../I18n/I18n'
 
 // Apply config overrides
 applyConfigSettings()
