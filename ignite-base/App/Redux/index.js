8d3357dea2d085d317a373ee3b72f020e293d4d7
fvonhoven <frank@infinite.red>
2016-11-08 11:27:22 -0600

    Make SearchRedux and add to Index

--- ignite-base/App/Redux/index.js
+++ ignite-base/App/Redux/index.js
@@ -8,7 +8,8 @@ export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
     temperature: require('./TemperatureRedux').reducer,
-    login: require('./LoginRedux').reducer
+    login: require('./LoginRedux').reducer,
+    search: require('./SearchRedux').reducer
   })
 
   return configureStore(rootReducer, rootSaga)
