d0379f38be9330cb9df913009a7d8797ff8c390c
fvonhoven <frank@infinite.red>
2016-11-08 11:31:09 -0600

    Clean up & refactor SearchBar

--- ignite-base/App/Components/SearchBar.js
+++ ignite-base/App/Components/SearchBar.js
@@ -1,70 +1,22 @@
 import React from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import styles from './Styles/SearchBarStyle'
-import { connect } from 'react-redux'
-// import Actions from '../Actions/Creators'
 import I18n from 'react-native-i18n'
 import { Colors, Metrics } from '../Themes/'
 import * as Animatable from 'react-native-animatable'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import R from 'ramda'
 
-class SearchBar extends React.Component {
+export default class SearchBar extends React.Component {
 
   static propTypes = {
-    onPress: React.PropTypes.func,
-    onCancelPress: React.PropTypes.func
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      searchText: this.props.searchText,
-      onSearch: this.props.onSearch
-    }
-  }
-
-  componentWillReceiveProps (newProps) {
-    if (this.props.searchText !== newProps.searchText) {
-      this.setState({
-        searchText: newProps.searchText
-      })
-    }
-  }
-
-  shouldComponentUpdate (newProps, newState) {
-    return R.not(R.equals(this.state, newState)) || R.not(R.equals(this.props, newProps))
-  }
-
-  handleChange = (searchText) => {
-    this.setState({searchText})
-    if (searchText.length < 3) {
-      // this.props.clearSearch()
-    } else {
-      setTimeout(() => {
-        this.refs.searchText.props.onSubmitEditing(this.state.searchText)
-      }, 300)
-    }
-  }
-
-  get value () {
-    return this.state.searchText
-  }
-
-  handleCancelSearch = () => {
-    this.props.onCancelPress()
-    // this.props.hideSearchModal()
-    // this.props.clearSearch()
-    this.refs.searchText.clear()
-  }
-
-  handleSearch () {
-    // this.props.performSearch(this.state.searchText)
-    // this.props.showSearchModal()
-    this.props.setSearchStatus(true)
+    onSearch: React.PropTypes.func.isRequired,
+    onCancel: React.PropTypes.func.isRequired,
+    searchTerm: React.PropTypes.string
   }
 
   render () {
+    const { onSearch, onCancel, searchTerm } = this.props
+    const onSubmitEditing = () => onSearch(searchTerm)
     return (
       <Animatable.View animation='slideInRight' duration={250} style={styles.container}>
         <TextInput
@@ -74,13 +26,12 @@ class SearchBar extends React.Component {
           placeholderTextColor={Colors.snow}
           underlineColorAndroid='transparent'
           style={styles.searchInput}
-          value={this.state.searchText}
-          onChangeText={(text) => this.handleChange(text)}
-          onSubmitEditing={() => this.handleSearch()}
-          returnKeyType={'done'}
+          value={this.props.searchTerm}
+          onChangeText={onSearch}
+          autoCapitalize='none'
+          onSubmitEditing={onSubmitEditing}
+          returnKeyType={'search'}
           autoCorrect={false}
-          clearButtonMode='unless-editing'
-          clearButtonColor='red'
           selectionColor={Colors.snow}
         />
         <View style={styles.searchIconContainer}>
@@ -90,27 +41,10 @@ class SearchBar extends React.Component {
             style={styles.searchIcon}
           />
         </View>
-        <TouchableOpacity onPress={this.handleCancelSearch} style={styles.cancelButton}>
-          <Text style={styles.buttonLabel}>Cancel</Text>
+        <TouchableOpacity onPress={onCancel} style={styles.cancelButton}>
+          <Text style={styles.buttonLabel}>{I18n.t('cancel')}</Text>
         </TouchableOpacity>
       </Animatable.View>
     )
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    // performSearch: (searchTerm) => dispatch(Actions.search(searchTerm)),
-    // setSearchStatus: (searchStatus) => dispatch(Actions.setSearchStatus(searchStatus)),
-    // clearSearch: () => dispatch(Actions.clearSearch()),
-    // showSearchModal: () => dispatch(Actions.showSearchModal()),
-    // hideSearchModal: () => dispatch(Actions.hideSearchModal())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBar)
