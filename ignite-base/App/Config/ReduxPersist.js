ee380213df0720f31a65314791a0c2f9d1c50f6e
fvonhoven <frank@infinite.red>
2016-11-08 11:31:47 -0600

    Bump reducer version & add search to persist blacklist

--- ignite-base/App/Config/ReduxPersist.js
+++ ignite-base/App/Config/ReduxPersist.js
@@ -3,10 +3,10 @@ import { AsyncStorage } from 'react-native'
 
 const REDUX_PERSIST = {
   active: true,
-  reducerVersion: '2',
+  reducerVersion: '4',
   storeConfig: {
     storage: AsyncStorage,
-    blacklist: ['login'], // reducer keys that you do NOT want stored to persistence here
+    blacklist: ['login', 'search'], // reducer keys that you do NOT want stored to persistence here
     // whitelist: [], Optionally, just specify the keys you DO want stored to
     // persistence. An empty array means 'don't store any reducers' -> infinitered/ignite#409
     transforms: [immutablePersistenceTransform]
