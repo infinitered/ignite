b3a1d586a22123ebe3964a8c095a18c135aa35d5
Steve Kellock <steve@kellock.ca>
2016-11-18 10:51:24 -0500

    Fix the console.tron call in a saga when running via ava.

--- ignite-base/App/Sagas/StartupSagas.js
+++ ignite-base/App/Sagas/StartupSagas.js
@@ -7,7 +7,7 @@ export const selectTemperature = (state) => state.temperature.temperature
 
 // process STARTUP actions
 export function * startup (action) {
-  if (__DEV__) {
+  if (__DEV__ && console.tron) {
     // straight-up string logging
     console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
 
