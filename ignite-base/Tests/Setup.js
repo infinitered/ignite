51d3ad3fc27975a677a2540ff17c10f25d0ce1e9
Derek Greenberg <derekg@gmail.com>
2016-11-30 16:56:02 -0800

    Add I18N support for Ava tests (#518)
    * Add I18N support for Ava tests
    
    * update commented codde in /Tests/Setup
    
    * fix missing updates to TemperatureSagaTest.js

--- ignite-base/Tests/Setup.js
+++ ignite-base/Tests/Setup.js
@@ -1,5 +1,7 @@
 import mockery from 'mockery'
 import m from 'module'
+import english from '../App/I18n/english.json'
+import {keys, replace, forEach} from 'ramda'
 
 // inject __DEV__ as it is not available when running through the tests
 global.__DEV__ = true
@@ -20,9 +22,22 @@ mockery.registerMock('reactotron-apisauce', {})
 mockery.registerMock('react-native-animatable', {View: 'Animatable.View'})
 mockery.registerMock('react-native-vector-icons/Ionicons', {})
 
-// mock i18n as it uses react native stufff
+// Mock i18n as it uses react native stuff
+// This mock returns the interpolated text from the english.json file in App/I18n
+// If you are not using '../App/I18n/english.json' for your I18n values, simply replace import english
+// with the import at the top of this file from '../App/I18n/english.json' with the I18n json file you
+// want to use, such as "import french from '../App/I18n/fr.json'" and set 'let value = french[key]`
 mockery.registerMock('react-native-i18n', {
-  t: (key) => key
+  t: (key, replacements) => {
+    let value = english[key]
+    if (!value) return key
+    if (!replacements) return value
+
+    forEach((r) => {
+      value = replace(`{{${r}}}`, replacements[r], value)
+    }, keys(replacements))
+    return value
+  }
 })
 
 // Mock all images for React Native
