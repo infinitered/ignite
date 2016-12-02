95c9d76cefb35553c2149d84f15e7d170d9cc8d9
derekgreenberg <derekg@gmail.com>
2016-12-02 10:53:21 -0800

    Remove references to Reactotron in the comments

--- ignite-base/App/Sagas/OpenScreenSagas.js
+++ ignite-base/App/Sagas/OpenScreenSagas.js
@@ -2,10 +2,10 @@ import { call } from 'redux-saga/effects'
 import { Actions as NavigationActions, ActionConst } from 'react-native-router-flux'
 import { merge } from 'ramda'
 
-// Process OPEN_SCREEN actions, intended for use with Reactotron to open a screen via a direct dispatch
+// Process OPEN_SCREEN actions
 export function * openScreen (action) {
   const {screen, options} = action
-  // Always reset the nav stack when opening a screen via Reactotron
+  // Always reset the nav stack when opening a screen by default
   // You can override the RESET type in the options passed to the OPEN_SCREEN dispatch
   const mergedOptions = merge({type: ActionConst.RESET}, options)
   yield call(NavigationActions[screen], mergedOptions)
