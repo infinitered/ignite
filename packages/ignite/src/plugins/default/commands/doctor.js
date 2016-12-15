3f4135108f654c2c383a0b98013ce7edb0cf50fd
Gant Laborde <GantMan@gmail.com>
2016-12-15 10:50:15 -0600

    add nits - no catch and use built in functions

--- packages/ignite/src/plugins/default/commands/doctor.js
+++ packages/ignite/src/plugins/default/commands/doctor.js
@@ -2,26 +2,26 @@ const R = require('ramda')
 const Shell = require('shelljs')
 
 module.exports = async function (context) {
-  try {
-    const { debug } = context.print
+  const { filesystem: { exists, read } } = context
+  const { debug } = context.print
 
-    // ... be the CLI you wish to see in the world
-    const platform = process.platform
-    const ignitePath = R.trim(Shell.which('ignite') || '')
-    const igniteVersion = R.trim(Shell.exec('ignite -V', { silent: true }).stdout)
-    const nodePath = R.trim(Shell.which('node'))
-    const npmVersion = R.trim(Shell.exec('npm --version', { silent: true }).stdout)
-    const npmPath = R.trim(Shell.which('npm'))
-    const nodeVersion = R.trim(Shell.exec('node --version', { silent: true }).stdout)
-    const ggPackageFile = `${process.cwd()}/node_modules/gluegun/package.json`
-    const ggVersion = Shell.test('-f', ggPackageFile) ? require(ggPackageFile).version : '¯\\_(ツ)_/¯'
+  // ... be the CLI you wish to see in the world
+  const platform = process.platform
+  const ignitePath = R.trim(Shell.which('ignite') || '')
+  const igniteVersion = R.trim(Shell.exec('ignite -V', { silent: true }).stdout)
+  const nodePath = R.trim(Shell.which('node'))
+  const npmVersion = R.trim(Shell.exec('npm --version', { silent: true }).stdout)
+  const npmPath = R.trim(Shell.which('npm'))
+  const nodeVersion = R.trim(Shell.exec('node --version', { silent: true }).stdout)
+  const ggPackageFile = `${process.cwd()}/node_modules/gluegun/package.json`
+  const ggVersion = exists(ggPackageFile) ? read(ggPackageFile, 'json').version : '¯\\_(ツ)_/¯'
 
-    const rnCli = R.split(/\s/, R.trim(Shell.exec('react-native --version', { silent: true }).stdout))[1] // lulz
+  const rnCli = R.split(/\s/, R.trim(Shell.exec('react-native --version', { silent: true }).stdout))[1] // lulz
 
-    const rnPackageFile = `${process.cwd()}/node_modules/react-native/package.json`
-    const appReactNativeVersion = Shell.test('-f', rnPackageFile) ? require(rnPackageFile).version : '¯\\_(ツ)_/¯'
+  const rnPackageFile = `${process.cwd()}/node_modules/react-native/package.json`
+  const appReactNativeVersion = Shell.test('-f', rnPackageFile) ? require(rnPackageFile).version : '¯\\_(ツ)_/¯'
 
-    const body = `
+  const body = `
 \`\`\`
 Computer
   Platform: ${platform}
@@ -42,8 +42,5 @@ App
   React Native Version: ${appReactNativeVersion}
 \`\`\`
 `
-    debug(body, 'Ignite Info')
-  } catch (e) {
-    console.dir(e)
-  }
+  debug(body, 'Ignite Info')
 }
