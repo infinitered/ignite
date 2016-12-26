b50adfbaf9cc172e9c8b08e3c4c9aacb3e2873a1
Gant <GantMan@gmail.com>
2016-12-25 19:30:26 -0600

    storyboard in comments

--- packages/ignite-vector-icons/index.js
+++ packages/ignite-vector-icons/index.js
@@ -1,9 +1,25 @@
+const vectorComponentExample = 'blah.js'
+
 const add = async function (context) {
+  const { print, filesystem, parameters, prompt } = context
+  const { info, warning, success, checkmark, error, debug } = print
   console.log('Adding Ignite Vector Icons')
+  debug(context)
+
+  // ideal
+  // // knows to add via npm/yarn with visual
+  // ignite.addModule('react-native-vector-icons')
+  // // copy examples - if examples are live
+  // ignite.addComponentExample(vectorComponentExample)
 }
 
 const remove = async function (context) {
   console.log('Removing Ignite Vector Icons')
+
+  // ideal
+  // ignite.removeModule('react-native-vector-icons')
+  // // remove examples
+  // ignite.removeComponentExample(vectorComponentExample)
 }
 
 module.exports = { add, remove }
