4c641a57105046c9bc8f5403e7fee0a22b4a71a3
Gant <GantMan@gmail.com>
2017-01-14 10:54:29 -0600

    start moving to proper templates

--- packages/ignite-basic-structure/index.js
+++ packages/ignite-basic-structure/index.js
@@ -2,41 +2,39 @@ const sourceFolder = `${process.cwd()}/node_modules/ignite-basic-structure/templ
 const generate = require('./shared/generate-utils')
 
 const add = async function (context) {
-  const { filesystem, parameters } = context
+  const { filesystem, parameters, ignite } = context
 
   const copyJobs = [
-    // {
-    //   template: 'index.ejs',
-    //   target: 'index.ios.js'
-    // },
-    // {
-    //   template: 'index.js',
-    //   target: 'index.android.js'
-    // },
-    // {
-    //   template: 'README.md',
-    //   target: 'README.md'
-    // },
-    // {
-    //   template: 'ignite.toml',
-    //   target: 'ignite.toml'
-    // },
-    // {
-    //   template: '.editorconfig',
-    //   target: '.editorconfig'
-    // },
-    // {
-    //   template: 'App',
-    //   target: 'App'
-    // }
+    {
+      template: 'index.ejs',
+      target: 'index.ios.js'
+    },
+    {
+      template: 'index.ejs',
+      target: 'index.android.js'
+    },
+    {
+      template: 'README.md',
+      target: 'README.md'
+    },
+    {
+      template: 'ignite.toml',
+      target: 'ignite/ignite.toml'
+    },
+    {
+      template: '.editorconfig',
+      target: '.editorconfig'
+    }
   ]
 
+  await ignite.copyBatch(context, copyJobs, {name: 'Taco'})
+
   // Should be using template.generate but we can't due to gluegun #126
-  filesystem.copy(`${sourceFolder}index.js`, `${process.cwd()}/index.ios.js`, { overwrite: true })
-  filesystem.copy(`${sourceFolder}index.js`, `${process.cwd()}/index.android.js`, { overwrite: true })
-  filesystem.copy(`${sourceFolder}README.md`, `${process.cwd()}/README.md`, { overwrite: true })
-  filesystem.copy(`${sourceFolder}ignite.toml`, `${process.cwd()}/ignite.toml`, { overwrite: true })
-  filesystem.copy(`${sourceFolder}.editorconfig`, `${process.cwd()}/.editorconfig`, { overwrite: true })
+  // filesystem.copy(`${sourceFolder}index.js`, `${process.cwd()}/index.ios.js`, { overwrite: true })
+  // filesystem.copy(`${sourceFolder}index.js`, `${process.cwd()}/index.android.js`, { overwrite: true })
+  // filesystem.copy(`${sourceFolder}README.md`, `${process.cwd()}/README.md`, { overwrite: true })
+  // filesystem.copy(`${sourceFolder}ignite.toml`, `${process.cwd()}/ignite/ignite.toml`, { overwrite: true })
+  // filesystem.copy(`${sourceFolder}.editorconfig`, `${process.cwd()}/.editorconfig`, { overwrite: true })
 
   if (parameters.options.unholy) {
     // copy far too many opinions
