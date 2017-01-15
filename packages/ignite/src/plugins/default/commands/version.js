bbb4490138813a71b08fd8af1a170042488f7bc2
Gant Laborde <GantMan@gmail.com>
2017-01-15 16:50:30 -0600

    add version command

---
+++ packages/ignite/src/plugins/default/commands/version.js
@@ -0,0 +1,8 @@
+// @cliDescription  Prints current version of installed ignite
+// ----------------------------------------------------------------------------
+//const pjson = require(`../../../package.json`)
+const pjson = require('./../../../../package.json')
+
+module.exports = async function (context) {
+  context.print.info(pjson.version)
+}
