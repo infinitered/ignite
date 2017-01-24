d4bcac974bfd5e7093b880f551d692b3506cee5a
Kevin VanGelder <kevin@infinite.red>
2017-01-23 16:28:41 -0800

    move launch screen and upgrade plugin examples

--- packages/ignite-basic-structure/templates/Unholy/Services/ExamplesRegistry.js
+++ packages/ignite-basic-structure/templates/Unholy/Services/ExamplesRegistry.js
@@ -5,11 +5,14 @@ import { Text, View } from 'react-native'
 import R from 'ramda'
 import { ApplicationStyles } from '../Themes'
 import DebugConfig from '../Config/DebugConfig'
-let globalExamplesRegistry = []
+let globalComponentExamplesRegistry = []
+let globalPluginExamplesRegistry = []
 
-export const addExample = (title: string, usage: () => React$Element<*>) => { if (DebugConfig.includeExamples) globalExamplesRegistry.push({title, usage}) }
+export const addComponentExample = (title: string, usage: () => React$Element<*>) => { if (DebugConfig.includeExamples) globalComponentExamplesRegistry.push({title, usage}) }
 
-const renderExample = (example: Object) => {
+export const addPluginExample = (title: string, usage: () => React$Element<*>) => { if (DebugConfig.includeExamples) globalPluginExamplesRegistry.push({title, usage})}
+
+const renderComponentExample = (example: Object) => {
   return (
     <View key={example.title}>
       <View style={ApplicationStyles.darkLabelContainer}>
@@ -20,10 +23,26 @@ const renderExample = (example: Object) => {
   )
 }
 
-export const renderExamples = () => R.map(renderExample, globalExamplesRegistry)
+const renderPluginExample = (example: Object) => {
+  return (
+    <View key={example.title}>
+      <View style={ApplicationStyles.darkLabelContainer}>
+        <Text style={ApplicationStyles.darkLabel}>{example.title}</Text>
+      </View>
+      {example.usage.call()}
+    </View>
+  )
+}
+
+export const renderComponentExamples = () => R.map(renderComponentExample, globalComponentExamplesRegistry)
+
+export const renderPluginExamples = () => R.map(renderPluginExample, globalPluginExamplesRegistry)
+
 
 // Default for readability
 export default {
-  render: renderExamples,
-  add: addExample
+  renderComponentExamples,
+  addComponentExample,
+  renderPluginExamples,
+  addPluginExample
 }
