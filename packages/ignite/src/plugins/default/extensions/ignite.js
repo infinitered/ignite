e47fe665c1f9cf2af3061e6e8f564f1665e383f6
Jamon Holmgren <jamon@infinite.red>
2017-01-25 14:28:11 -0800

    WIP - ignite-i18n plugin

--- packages/ignite/src/plugins/default/extensions/ignite.js
+++ packages/ignite/src/plugins/default/extensions/ignite.js
@@ -308,6 +308,28 @@ function attach (plugin, command, context) {
     }
   }
 
+  /**
+   * Conditionally inserts a string into a file before or after another string.
+   * TODO: Move to infinitered/gluegun eventually? Plugin or core?
+   *
+   * @param {string}  file            File to be patched
+   * @param {Object}  opts            Options
+   * @param {string}  opts.before     Insert before this string
+   * @param {string}  opts.after      Insert after this string
+   * @param {string}  opts.insert     String to be inserted
+   * @param {string}  opts.match      Skip if this string exists already
+   *
+   * @example
+   *   patchInFile('thing.js', { before: 'bar', insert: 'foo' })
+   *
+   */
+  function patchInFile (file, opts) {
+    const { patching } = context
+    if (!patching.isInFile(file, opts.match || opts.insert)) {
+      patching.insertInFile(file, opts.before || opts.after, opts.insert, !!opts.after)
+    }
+  }
+
   // send back the extension
   return {
     ignitePluginPath,
@@ -322,7 +344,8 @@ function attach (plugin, command, context) {
     setGlobalConfig,
     removeGlobalConfig,
     setDebugConfig,
-    removeDebugConfig
+    removeDebugConfig,
+    patchInFile
   }
 }
 
