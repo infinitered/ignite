733263653bec6101aa22776e5fd10b7a76b64fab
Jamon Holmgren <jamon@infinite.red>
2017-02-09 21:28:27 -0800

    WIP: ignite plugin new

--- packages/ignite/src/commands/plugin.js
+++ packages/ignite/src/commands/plugin.js
@@ -10,15 +10,27 @@ const exitCodes = require('../lib/exitCodes')
  * @param {Object} context The gluegun context.
  * @returns {Boolean}
  */
-const checkName = (context) => {
+const checkName = (pluginName, context) => {
   const { strings, print, runtime, parameters } = context
   
-  if (strings.isBlank(parameters.third)) {
+  if (strings.isBlank(pluginName)) {
     print.info(`${runtime.brand} plugin new <${runtime.brand}-plugin-name>\n`)
     print.error('Plugin name is required')
     process.exit(exitCodes.PLUGIN_NAME)
     return false
   }
+
+  if (pluginName.indexOf('ignite-') !== 0) {
+    print.info(`Ignite plugins typically start with ignite-* -- but you do you.`)
+  }
+
+  // TODO: Make this better at detecting invalid plugin names
+  if (!/^[a-z0-9].*/i.test(pluginName)) {
+    print.error('Plugin name should be a valid folder name')
+    process.exit(exitCodes.PLUGIN_NAME)
+    return false
+  }
+
   return true
 }
 
@@ -27,10 +39,31 @@ const checkName = (context) => {
  *
  * @param {Object} context The gluegun context.
  */
-const createNewPlugin = (context) => {
-  if (!checkName(context)) { return }
+const createNewPlugin = async (context) => {
+  const { parameters, print, ignite, strings, filesystem } = context
 
-  context.print.error(`not done yet`)
+  const pluginName = parameters.third
+  if (!checkName(pluginName, context)) { return }
+
+  const name = strings.pascalCase(pluginName.replace(/^ignite-/, ""))
+
+  print.info(`Creating new plugin: ${pluginName}`)
+
+  // filesystem.dir(`${pluginName}/templates`)
+
+  const copyJobs = [
+    {
+      template: 'plugin/index.js.ejs',
+      target: `${pluginName}/index.js`
+    },
+    {
+      template: 'plugin/templates/Example.js.ejs',
+      target: `${pluginName}/templates/${name}Example.js`
+    }
+  ]
+
+  // copy over the templates
+  await ignite.copyBatch(context, copyJobs, {name, pluginName})
 }
 
 /**
@@ -64,7 +97,7 @@ module.exports = async function (context) {
   // context.print.debug(context.parameters)
   switch (parameters.second) {
     case 'new':
-      createNewPlugin(context)
+      await createNewPlugin(context)
       break;
 
     case 'list':
