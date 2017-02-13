4dc34330848740ccb7fca42ed01910ba539ee05d
Gant Laborde <GantMan@gmail.com>
2017-02-13 11:54:05 -0600

    attempt at remove

--- packages/ignite-basic-generators/index.js
+++ packages/ignite-basic-generators/index.js
@@ -1,24 +1,28 @@
+const screenExamples = [
+  {
+    title: 'Row Example', 
+    screen: 'examples/RowExample.js', 
+    ancillary: ['examples/Styles/RowExampleStyle.js']
+  },{
+    title: 'Grid Example', 
+    screen: 'examples/GridExample.js', 
+    ancillary: ['examples/Styles/GridExampleStyle.js']
+  },{
+    title: 'Sections Example', 
+    screen: 'examples/SectionExample.js', 
+    ancillary: ['examples/Styles/SectionExampleStyle.js']
+  }
+]
+
 const add = async function (context) {
   // examples of generated screens
-  await context.ignite.addScreenExamples([
-    {
-      title: 'Row Example', 
-      screen: 'examples/RowExample.js', 
-      ancillary: ['examples/Styles/RowExampleStyle.js']
-    },{
-      title: 'Grid Example', 
-      screen: 'examples/GridExample.js', 
-      ancillary: ['examples/Styles/GridExampleStyle.js']
-    },{
-      title: 'Sections Example', 
-      screen: 'examples/SectionExample.js', 
-      ancillary: ['examples/Styles/SectionExampleStyle.js']
-    }
-  ])
+  await context.ignite.addScreenExamples(screenExamples)
 }
 
 const remove = async function (context) {
-  // No-op -- we are generator-only.
+  console.log('I got called')
+  // remove screens
+  await context.ignite.removeScreenExamples(screenExamples)
 }
 
 module.exports = { add, remove }
--- packages/ignite/src/extensions/ignite.js
+++ packages/ignite/src/extensions/ignite.js
@@ -183,17 +183,17 @@ function attach (plugin, command, context) {
   }
 
   /**
-   * Generates an example screen for use with the dev screens.
+   * Generates example screens for in dev screens.
    *
    * @param {Array} files - Array of Screens and properties
    * @param {Object} props - The properties to use for template expansion.
    * 
    * example:
-   * copyScreenExamples([
+   * addScreenExamples([
    *   {title: 'Row Example', screen: 'Row.js', ancillary: ['file1', 'file2']},
    *   {title: 'Grid Example', screen: 'Grid.js', ancillary: ['file']},
    *   {title: 'Section Example', screen: 'Section.js', ancillary: ['file']},
-   * ], 'Group Title')
+   * ])
    */
   async function addScreenExamples (files, props = {}) {
     const { filesystem, patching, ignite } = context
@@ -203,7 +203,7 @@ function attach (plugin, command, context) {
 
     // currently only supporting 1 form of examples
     if (config.examples === 'classic') {
-      const spinner = print.spin(`▸ adding screen example`)
+      const spinner = print.spin(`▸ adding screen examples`)
 
       // merge and flatten all dem files yo.
       let allFiles = reduce((acc, v) => {
@@ -254,8 +254,7 @@ function attach (plugin, command, context) {
             `
             <RoundedButton onPress={() => this.props.navigation.navigate('${componentName}')}>
               ${file.title}
-            </RoundedButton>            
-            `
+            </RoundedButton>`
           )
         } // if
       }, files)      
@@ -263,6 +262,79 @@ function attach (plugin, command, context) {
       spinner.stop()
     }
   }
+
+  /**
+   * Remove example screens from dev screens.
+   *
+   * @param {Array} files - Array of Screens and properties
+   * 
+   * example:
+   * removeScreenExamples([
+   *   {screen: 'Row.js', ancillary: ['file1', 'file2']},
+   *   {screen: 'Grid.js', ancillary: ['file']},
+   *   {screen: 'Section.js', ancillary: ['file']},
+   * ])
+   */
+  async function removeScreenExamples (files) {
+    const { filesystem, patching, ignite } = context
+    const config = ignite.loadIgniteConfig()
+    // consider this being part of context.ignite
+    const pluginName = takeLast(1, split(path.sep, ignitePluginPath()))[0]
+
+    // currently only supporting 1 form of examples
+    if (config.examples === 'classic') {
+      const spinner = print.spin(`▸ removing screen examples`)
+
+      // merge and flatten all dem files yo.
+      let allFiles = reduce((acc, v) => {
+        acc.push(v.screen)
+        if (v.ancillary) acc.push(v.ancillary)
+        return flatten(acc)
+      }, [], files)
+
+      // delete all files that were inserted
+      map((fileName) => {
+        filesystem.removeAsync(`ignite/Examples/Containers/${pluginName}/${fileName}`)       
+      }, allFiles)
+
+      // delete screen, route, and buttons in PluginExamples (if exists)
+      const destinationPath = `${process.cwd()}/ignite/DevScreens/PluginExamplesScreen.js`
+      map((file) => {
+        // turn things like "examples/This File-Example.js" into "ThisFileExample"
+        // for decent component names
+        const exampleFileName = takeLast(1, split(path.sep, file.screen))[0]        
+        const componentName = replace(/.js|\s|-/g, '', exampleFileName)
+
+        if (filesystem.exists(destinationPath)) {
+          // insert screen import
+          patching.replaceInFile(
+            destinationPath, 
+            `import ${componentName} from '../Examples/Containers/${pluginName}/${file.screen}'`,
+            ''             
+          )
+
+          // insert screen route 
+          patching.replaceInFile(
+            destinationPath, 
+            `  ${componentName}: {screen: ${componentName}, navigationOptions: {header: {visible: true}}},`,
+            ''
+          )
+
+          // insert launch button 
+          patching.replaceInFile(
+            destinationPath, 
+            `
+            <RoundedButton onPress={() => this.props.navigation.navigate('${componentName}')}>
+              ${file.title}
+            </RoundedButton>`,
+            ''
+          )
+        } // if
+      }, files)      
+ 
+      spinner.stop()
+    }
+  }  
 
   /**
    * Generates an example for use with the dev screens.
@@ -439,6 +511,7 @@ function attach (plugin, command, context) {
     addComponentExample,
     removeComponentExample,
     addScreenExamples,
+    removeScreenExamples,
     loadIgniteConfig,
     saveIgniteConfig,
     setIgniteConfig,
