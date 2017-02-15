06fd96b9a4025f20c8a314399a16b3fdccbe788b
Steve Kellock <steve@kellock.ca>
2017-02-15 08:51:44 -0500

    Updates to React Native 0.41.2.

--- packages/ignite/src/commands/new.js
+++ packages/ignite/src/commands/new.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 // The default version of React Native to install. We will want to upgrade
 // this when we test out new releases and they work well with our setup.
-const REACT_NATIVE_VERSION = '0.41.1'
+const REACT_NATIVE_VERSION = '0.41.2'
 
 // Questions to ask during install if the chatty route is chosen.
 const installWalkthrough = [
