8a1905995c3c30add3d6a7192a78db1c79e77b84
Gant <GantMan@gmail.com>
2017-02-26 20:48:08 -0600

    fix tests and lint

--- packages/ignite-unholy-app-template/templates/App/Services/Api.js
+++ packages/ignite-unholy-app-template/templates/App/Services/Api.js
@@ -43,7 +43,7 @@ const create = (baseURL = 'https://api.github.com/') => {
   //
   const getRoot = () => api.get('')
   const getRate = () => api.get('rate_limit')
-  const getUser = (username) => api.get('search/users', {q: username})  
+  const getUser = (username) => api.get('search/users', {q: username})
 
   // ------
   // STEP 3
