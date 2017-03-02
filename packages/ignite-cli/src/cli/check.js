6d017531db016aca8b64e4a5db62bc6bebe515d8
Jamon Holmgren <jamon@infinite.red>
2017-03-02 05:16:00 -0800

    Requires Yarn 0.20.0+, if installed. (#808)
    * Requires Yarn 0.20.0+, if installed.
    
    * Fixes bug with yarn warning message.

--- packages/ignite-cli/src/cli/check.js
+++ packages/ignite-cli/src/cli/check.js
@@ -1,5 +1,6 @@
 // check the node version
 const { nodeVersion, isNewEnough, hasAsyncAwait } = require('gluegun/sniff')
+const Shell = require('shelljs')
 
 // check the node version
 if (!isNewEnough) {
@@ -11,5 +12,23 @@ if (!isNewEnough) {
 if (!hasAsyncAwait) {
   console.log(`The async feature is not available. Please ensure your Node is up to date.`)
   process.exit(2)
+}
+
+// check the yarn version is >= 0.20
+const getYarnVersion = () => {
+  return Shell.exec('yarn --version', {silent: true}).stdout.replace('\n', '').split('.')
+}
+
+const checkYarn = (yarnVersion) => {
+  const major = parseInt(yarnVersion[0], 10)
+  const minor = parseInt(yarnVersion[1], 10)
+  return (major >= 1) || (minor >= 20)
+}
+
+const hasYarn = Shell.which('yarn')
+if (hasYarn && !checkYarn(getYarnVersion())) {
+  console.log(`You have yarn installed but it's version ${getYarnVersion().join('.')}. Ignite requires yarn 20.0+.`)
+  console.log('Run `brew upgrade yarn` if you use Homebrew or visit https://yarnpkg.com/en/docs/install.')
+  process.exit(3)
 }
 
