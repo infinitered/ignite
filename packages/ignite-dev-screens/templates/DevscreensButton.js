17555b783a2b4f32c9a4f736b91a4a70b03a90f8
mwarger <mwarger@gmail.com>
2017-03-07 00:14:20 -0600

    updated to fullfill propTypes contract removed propTypes validation error per https://facebook.github.io/react-native/docs/modal.html#onrequestclose

--- packages/ignite-dev-screens/templates/DevscreensButton.js
+++ packages/ignite-dev-screens/templates/DevscreensButton.js
@@ -1,35 +1,40 @@
-import React from 'react'
-import { View, Modal } from 'react-native'
-import DebugConfig from '../../App/Config/DebugConfig'
-import RoundedButton from '../../App/Components/RoundedButton'
-import PresentationScreen from './PresentationScreen'
+import React from 'react';
+import { View, Modal, Platform } from 'react-native';
+import DebugConfig from '../../App/Config/DebugConfig';
+import RoundedButton from '../../App/Components/RoundedButton';
+import PresentationScreen from './PresentationScreen';
 
 export default class DevscreensButton extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       showModal: false
-    }
+    };
   }
 
   toggleModal = () => {
-    this.setState({showModal: !this.state.showModal})
-  }
+    this.setState({ showModal: !this.state.showModal });
+  };
 
-  render () {
+  render() {
     if (DebugConfig.showDevScreens) {
       return (
         <View>
           <RoundedButton onPress={this.toggleModal}>
             Open DevScreens
           </RoundedButton>
-          <Modal visible={this.state.showModal} >
-            <PresentationScreen screenProps={{toggle: this.toggleModal}} />
+          <Modal
+            visible={this.state.showModal}
+            onRequestClose={
+              Platform.OS === 'android' ? () => this.toggleModal() : undefined
+            }
+          >
+            <PresentationScreen screenProps={{ toggle: this.toggleModal }} />
           </Modal>
         </View>
-      )
+      );
     } else {
-      return (<View />)
+      return <View />;
     }
   }
 }
