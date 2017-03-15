5c54d975474210da4d711480a217c6767b46ae41
Jamon Holmgren <jamon@infinite.red>
2017-03-15 15:55:13 -0700

    Refactors to remove some unnecessary lines

--- packages/ignite-cli/src/cli/check.js
+++ packages/ignite-cli/src/cli/check.js
@@ -1,18 +1,15 @@
 // check the node version
 var Sniff = require('gluegun/sniff')
-var nodeVersion = Sniff.nodeVersion
-var isNewEnough = Sniff.isNewEnough
-var hasAsyncAwait = Sniff.hasAsyncAwait
 var Shell = require('shelljs')
 
 // check the node version
-if (!isNewEnough) {
-  console.log('Node.js 7.6+ is required to run. You have ' + nodeVersion + '. Womp, womp.')
+if (!Sniff.isNewEnough) {
+  console.log('Node.js 7.6+ is required to run. You have ' + Sniff.nodeVersion + '. Womp, womp.')
   process.exit(1)
 }
 
 // check for async and await
-if (!hasAsyncAwait) {
+if (!Sniff.hasAsyncAwait) {
   console.log('The async feature is not available. Please ensure your Node is up to date.')
   process.exit(2)
 }
