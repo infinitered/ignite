8ac853fc2a194a072a2e1538d234f75eda3dcf25
Jamon Holmgren <jamon@infinite.red>
2017-03-15 17:48:15 -0700

    Converts check.js to ES3 syntax (#903)
    * Converts check.js to ES3 syntax
    
    * Refactors to remove some unnecessary lines

--- packages/ignite-cli/src/cli/check.js
+++ packages/ignite-cli/src/cli/check.js
@@ -1,33 +1,33 @@
 // check the node version
-const { nodeVersion, isNewEnough, hasAsyncAwait } = require('gluegun/sniff')
-const Shell = require('shelljs')
+var Sniff = require('gluegun/sniff')
+var Shell = require('shelljs')
 
 // check the node version
-if (!isNewEnough) {
-  console.log(`Node.js 7.6+ is required to run. You have ${nodeVersion}. Womp, womp.`)
+if (!Sniff.isNewEnough) {
+  console.log('Node.js 7.6+ is required to run. You have ' + Sniff.nodeVersion + '. Womp, womp.')
   process.exit(1)
 }
 
 // check for async and await
-if (!hasAsyncAwait) {
-  console.log(`The async feature is not available. Please ensure your Node is up to date.`)
+if (!Sniff.hasAsyncAwait) {
+  console.log('The async feature is not available. Please ensure your Node is up to date.')
   process.exit(2)
 }
 
 // check the yarn version is >= 0.20
-const getYarnVersion = () => {
+function getYarnVersion () {
   return Shell.exec('yarn --version', {silent: true}).stdout.replace('\n', '').split('.')
 }
 
-const checkYarn = (yarnVersion) => {
-  const major = parseInt(yarnVersion[0], 10)
-  const minor = parseInt(yarnVersion[1], 10)
+function checkYarn (yarnVersion) {
+  var major = parseInt(yarnVersion[0], 10)
+  var minor = parseInt(yarnVersion[1], 10)
   return (major >= 1) || (minor >= 20)
 }
 
-const hasYarn = Shell.which('yarn')
+var hasYarn = Shell.which('yarn')
 if (hasYarn && !checkYarn(getYarnVersion())) {
-  console.log(`You have yarn installed but it's version ${getYarnVersion().join('.')}. Ignite requires yarn 20.0+.`)
+  console.log('You have yarn installed but it\'s version ' + getYarnVersion().join('.') + '. Ignite requires yarn 20.0+.')
   console.log('Run `brew upgrade yarn` if you use Homebrew or visit https://yarnpkg.com/en/docs/install.')
   process.exit(3)
 }
