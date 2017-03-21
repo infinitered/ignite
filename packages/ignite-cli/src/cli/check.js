f1bd1d90a395c39c13439274efed3c0c7ecdf58b
Steve Kellock <steve@kellock.ca>
2017-03-21 11:07:29 -0400

    Enforces global dependencies are available.

--- packages/ignite-cli/src/cli/check.js
+++ packages/ignite-cli/src/cli/check.js
@@ -1,34 +1,52 @@
+// NOTE:  this file is intentionally written with es3
+
 // check the node version
 var Sniff = require('gluegun/sniff')
-var Shell = require('shelljs')
 
 // check the node version
 if (!Sniff.isNewEnough) {
-  console.log('Node.js 7.6+ is required to run. You have ' + Sniff.nodeVersion + '. Womp, womp.')
+  console.log(
+    'Node.js 7.6+ is required to run. You have ' +
+      Sniff.nodeVersion +
+      '. Womp, womp.'
+  )
   process.exit(1)
 }
 
 // check for async and await
 if (!Sniff.hasAsyncAwait) {
-  console.log('The async feature is not available. Please ensure your Node is up to date.')
+  console.log(
+    'The async feature is not available. Please ensure your Node is up to date.'
+  )
   process.exit(2)
 }
 
-// check the yarn version is >= 0.20
-function getYarnVersion () {
-  return Shell.exec('yarn --version', {silent: true}).stdout.replace('\n', '').split('.')
+// lets check our global dependencies
+var enforceGlobalDependency = require('./enforceGlobalDependency')
+var exitCodes = require('../lib/exitCodes')
+
+// react-native-cli
+var rnCli = enforceGlobalDependency({
+  optional: false,
+  range: '>=2.0.0',
+  which: 'react-native',
+  versionCommand: 'react-native --version',
+  installMessage: 'To install: npm i -g react-native-cli'
+})
+
+if (!rnCli) {
+  process.exit(exitCodes.INVALID_GLOBAL_DEPENDENCY)
 }
 
-function checkYarn (yarnVersion) {
-  var major = parseInt(yarnVersion[0], 10)
-  var minor = parseInt(yarnVersion[1], 10)
-  return (major >= 1) || (minor >= 20)
-}
+// yarn
+var yarn = enforceGlobalDependency({
+  optional: true,
+  range: '>=0.21.0',
+  which: 'yarn',
+  versionCommand: 'yarn --version',
+  installMessage: 'See https://yarnpkg.com/en/docs/install on how to upgrade for your OS.'
+})
 
-var hasYarn = Shell.which('yarn')
-if (hasYarn && !checkYarn(getYarnVersion())) {
-  console.log('You have yarn installed but it\'s version ' + getYarnVersion().join('.') + '. Ignite requires yarn 20.0+.')
-  console.log('Run `brew upgrade yarn` if you use Homebrew or visit https://yarnpkg.com/en/docs/install.')
-  process.exit(3)
+if (!yarn) {
+  process.exit(exitCodes.INVALID_GLOBAL_DEPENDENCY)
 }
-
