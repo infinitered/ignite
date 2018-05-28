5deda75f5b1f6d0978a5e6cdda26975e4c26a5d6
Adrien Thiery <adrien.thiery@osedea.com>
2018-05-28 14:39:19 -0400

    Lint code

--- src/commands/boilerplatize.js
+++ src/commands/boilerplatize.js
@@ -2,25 +2,25 @@
 // @cliAlias bz
 // ----------------------------------------------------------------------------
 
-const ignore = require('ignore');
+const ignore = require('ignore')
 
-const targetFolder = 'boilerplate/';
-let projectName;
+const targetFolder = 'boilerplate/'
+let projectName
 let ignoredFiles = ignore().add([
   '.DS_Store',
   'boilerplate',
-  '.git',
-]);
+  '.git'
+])
 
 /**
  * @param {string} fileName - Name of the file we want to copy
  * @param {GluegunContext} context - the Gluegun context
  */
-async function generateFile(relativePath, context) {
-  const { filesystem, print: { info, colors }, patching, ignite } = context;
-  const absolutePath = `${filesystem.cwd()}/${relativePath}`;
+async function generateFile (relativePath, context) {
+  const { filesystem, ignite } = context
+  const absolutePath = `${filesystem.cwd()}/${relativePath}`
 
-  const fileContent = filesystem.read(absolutePath);
+  const fileContent = filesystem.read(absolutePath)
 
   // Checking if file contains imports from the project
   const fileIncludesProjectName =
@@ -28,21 +28,21 @@ async function generateFile(relativePath, context) {
     fileContent.includes(`require("${projectName}`) ||
     fileContent.includes(`from '${projectName}/`) ||
     fileContent.includes(`from "${projectName}/`) ||
-    fileContent.includes(`"${projectName}"`);
+    fileContent.includes(`"${projectName}"`)
 
   if (fileIncludesProjectName) {
     if (relativePath === 'package.json') {
-      const packageJSON = filesystem.read(absolutePath, 'json');
-      delete packageJSON.dependencies.react;
-      delete packageJSON.dependencies['react-native'];
+      const packageJSON = filesystem.read(absolutePath, 'json')
+      delete packageJSON.dependencies.react
+      delete packageJSON.dependencies['react-native']
       filesystem.write(`${targetFolder}${relativePath}.ejs`, packageJSON, { atomic: true })
     } else {
-      filesystem.copy(absolutePath, `${targetFolder}${relativePath}.ejs`);
+      filesystem.copy(absolutePath, `${targetFolder}${relativePath}.ejs`)
     }
-    console.log(`Patching ${targetFolder}${relativePath}.ejs`);
-    await ignite.patchInFile(`${targetFolder}${relativePath}.ejs`, { replace: new RegExp(projectName, 'g'), insert: '<% props.name %>' });
+    console.log(`Patching ${targetFolder}${relativePath}.ejs`)
+    await ignite.patchInFile(`${targetFolder}${relativePath}.ejs`, { replace: new RegExp(projectName, 'g'), insert: '<% props.name %>' })
   } else {
-    filesystem.copy(absolutePath, `${targetFolder}${relativePath}`);
+    filesystem.copy(absolutePath, `${targetFolder}${relativePath}`)
   }
 };
 
@@ -50,30 +50,29 @@ async function generateFile(relativePath, context) {
  * @param {string} folderName - Name of the folder we want to list and copy
  * @param {GluegunContext} context - the Gluegun context
  */
-async function listAndCopy(folderName, context, acceptedForSubdirectories = false) {
-  const isFirstLoop = !folderName;
-  const { ignite, filesystem, print: { colors, info } } = context;
-  const relativeFolderPathExceptForRoot = isFirstLoop ? filesystem.cwd() : `${folderName}/`;
-  const absoluteFolderPath = !isFirstLoop ? `${filesystem.cwd()}/${relativeFolderPathExceptForRoot}` : relativeFolderPathExceptForRoot;
+async function listAndCopy (folderName, context, acceptedForSubdirectories = false) {
+  const isFirstLoop = !folderName
+  const { ignite, filesystem, print: { colors, info } } = context
+  const relativeFolderPathExceptForRoot = isFirstLoop ? filesystem.cwd() : `${folderName}/`
 
   if (ignoredFiles.ignores(relativeFolderPathExceptForRoot)) {
-    ignite.log(`Ignoring ${relativeFolderPathExceptForRoot}`);
-    return;
+    ignite.log(`Ignoring ${relativeFolderPathExceptForRoot}`)
+    return
   }
 
   // handleiOSAndAndroidFolders()
-  const files = await filesystem.list(relativeFolderPathExceptForRoot);
+  const files = await filesystem.list(relativeFolderPathExceptForRoot)
 
   for (let index = 0; index < files.length; index++) {
-    const item = files[index];
-    const srcItemRelativePath = `${folderName ? `${folderName}/` : ''}${item}`;
-    ignite.log(`Checking: ${srcItemRelativePath}`);
+    const item = files[index]
+    const srcItemRelativePath = `${folderName ? `${folderName}/` : ''}${item}`
+    ignite.log(`Checking: ${srcItemRelativePath}`)
 
     if (ignoredFiles.ignores(srcItemRelativePath)) {
-      ignite.log(`Ignoring ${srcItemRelativePath}`);
+      ignite.log(`Ignoring ${srcItemRelativePath}`)
     } else {
-      if (filesystem.exists(srcItemRelativePath) === "dir") {
-        let answer;
+      if (filesystem.exists(srcItemRelativePath) === 'dir') {
+        let answer
 
         if (!acceptedForSubdirectories) {
           answer = await context.prompt.ask([
@@ -82,21 +81,21 @@ async function listAndCopy(folderName, context, acceptedForSubdirectories = fals
               message: `Do you want to include the '${srcItemRelativePath}' folder in your boilerplate?`,
               type: 'radio',
               choices: ['Yes', 'Yes and all the subdirectories', 'No']
-            },
-          ]);
+            }
+          ])
         } else {
-          answer = { includeFolder: 'Yes and all the subdirectories' };
+          answer = { includeFolder: 'Yes and all the subdirectories' }
         }
 
         if (answer.includeFolder.includes('Yes')) {
-          filesystem.dir(`${targetFolder}${srcItemRelativePath}`);
-          info(colors.green(`Creating directory: ${targetFolder}${srcItemRelativePath}`));
-          await listAndCopy(srcItemRelativePath, context, answer.includeFolder === 'Yes and all the subdirectories');
+          filesystem.dir(`${targetFolder}${srcItemRelativePath}`)
+          info(colors.green(`Creating directory: ${targetFolder}${srcItemRelativePath}`))
+          await listAndCopy(srcItemRelativePath, context, answer.includeFolder === 'Yes and all the subdirectories')
         } else {
-          info(colors.yellow(`Skipping directory: ${srcItemRelativePath}`));
+          info(colors.yellow(`Skipping directory: ${srcItemRelativePath}`))
         }
       } else {
-        await generateFile(srcItemRelativePath, context);
+        await generateFile(srcItemRelativePath, context)
       }
     }
   }
@@ -105,29 +104,29 @@ async function listAndCopy(folderName, context, acceptedForSubdirectories = fals
 /**
  * @param {GluegunContext} context - the Gluegun context
  */
-async function boilerplatize(context) {
-  const { print: { colors, info }, ignite, filesystem } = context;
+async function boilerplatize (context) {
+  const { print: { colors, info }, ignite, filesystem } = context
 
-  ignite.log('running boilerplatize command');
+  ignite.log('running boilerplatize command')
 
   // Get the project name out of package.json
-  const packageJSON = filesystem.read(`${filesystem.cwd()}/package.json`, 'json');
-  projectName = packageJSON.name;
+  const packageJSON = filesystem.read(`${filesystem.cwd()}/package.json`, 'json')
+  projectName = packageJSON.name
 
   // Read gitignore and filter comments and empty lines
-  const gitignore = filesystem.read(`${filesystem.cwd()}/.gitignore`).split(filesystem.eol).filter((item) => item[0] !== '#' && item !== '');
+  const gitignore = filesystem.read(`${filesystem.cwd()}/.gitignore`).split(filesystem.eol).filter((item) => item[0] !== '#' && item !== '')
 
-  ignoredFiles.add(gitignore);
+  ignoredFiles.add(gitignore)
 
   if (filesystem.exists(`${targetFolder}`)) {
-    info(colors.green(`Removing ${targetFolder}`));
-    filesystem.remove(`${targetFolder}`);
+    info(colors.green(`Removing ${targetFolder}`))
+    filesystem.remove(`${targetFolder}`)
   }
 
-  info(colors.green(`Creating ${targetFolder}`));
-  filesystem.dir(`${targetFolder}`);
+  info(colors.green(`Creating ${targetFolder}`))
+  filesystem.dir(`${targetFolder}`)
 
-  await listAndCopy(null, context);
+  await listAndCopy(null, context)
 }
 
-module.exports = boilerplatize;
+module.exports = boilerplatize
