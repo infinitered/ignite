bb64cfae9dba8fec83bb10281f33dad6492e85b1
Jamon Holmgren <jamon@infinite.red>
2018-12-05 09:22:58 -0800

    Bump

--- tests/integration/ignite-new/new.test.js
+++ tests/integration/ignite-new/new.test.js
@@ -9,6 +9,7 @@ jest.setTimeout(90 * 1000)
 
 const originalDir = process.cwd()
 const tempDir = tempy.directory()
+const appDir = `${tempDir}/${APP_DIR}`
 
 // beforeEach(() => process.chdir(tempDir))
 // afterEach(() => process.chdir(originalDir))
@@ -18,25 +19,25 @@ test('spins up a min app and performs various checks', async done => {
   // process.chdir(APP_DIR)
 
   // check the contents of ignite/ignite.json
-  const igniteJSON = jetpack.read(`${tempDir}/ignite/ignite.json`)
+  const igniteJSON = jetpack.read(`${appDir}/ignite/ignite.json`)
   expect(typeof igniteJSON).toBe('string')
   expect(igniteJSON).toMatch(/"generators": {/)
 
   // check the Containers/App.js file
-  const appJS = jetpack.read(`${tempDir}/App/Containers/App.js`)
+  const appJS = jetpack.read(`${appDir}/App/Containers/App.js`)
   expect(appJS).toMatch(/class App extends Component {/)
 
   // run ignite g component
-  await system.spawn(`${IGNITE} g component Test`)
+  await system.spawn(`cd ${appDir} && ${IGNITE} g component Test`)
   expect(jetpack.inspect('App/Components/Test.js').type).toBe('file')
 
   // spork a screen and edit it
-  await system.spawn(`cd ${tempDir} && ${IGNITE} spork component.ejs`)
-  const sporkedFile = `${tempDir}/ignite/Spork/ignite-ir-boilerplate-andross/component.ejs`
+  await system.spawn(`cd ${appDir} && ${IGNITE} spork component.ejs`)
+  const sporkedFile = `${appDir}/ignite/Spork/ignite-ir-boilerplate-andross/component.ejs`
   await jetpack.write(sporkedFile, 'SPORKED!')
   expect(jetpack.inspect(sporkedFile).type).toBe('file')
-  await system.spawn(`cd ${tempDir} && ${IGNITE} generate component Sporkified`)
-  expect(jetpack.read('App/Components/Sporkified.js')).toBe('SPORKED!')
+  await system.spawn(`cd ${appDir} && ${IGNITE} generate component Sporkified`)
+  expect(jetpack.read(`${appDir}/App/Components/Sporkified.js`)).toBe('SPORKED!')
 
   done()
 
