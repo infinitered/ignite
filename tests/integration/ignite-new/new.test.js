d42555cb38f4852e30d544117ca7f0ee101dbcd9
Jamon Holmgren <jamon@infinite.red>
2018-12-05 09:57:28 -0800

    Fix integration

--- tests/integration/ignite-new/new.test.js
+++ tests/integration/ignite-new/new.test.js
@@ -1,43 +1,41 @@
-const { system } = require('gluegun')
+const { system, filesystem } = require('gluegun')
 const tempy = require('tempy')
-const jetpack = require('fs-jetpack')
 
 const IGNITE = `${process.cwd()}/bin/ignite`
-const APP_DIR = 'Foo'
+const APP_NAME = 'Foo'
 
-jest.setTimeout(90 * 1000)
+jest.setTimeout(10 * 60 * 1000)
 
 const originalDir = process.cwd()
 const tempDir = tempy.directory()
-const appDir = `${tempDir}/${APP_DIR}`
+const appDir = `${tempDir}/${APP_NAME}`
+const opts = { cwd: appDir, stdio: 'inherit' }
 
 // beforeEach(() => process.chdir(tempDir))
 // afterEach(() => process.chdir(originalDir))
 
 test('spins up a min app and performs various checks', async done => {
-  await system.spawn(`cd ${tempDir} && ${IGNITE} new ${APP_DIR} --min -b ignite-ir-boilerplate-andross`)
-  // process.chdir(APP_DIR)
-
+  await system.spawn(`${IGNITE} new ${APP_NAME} --min -b ignite-ir-boilerplate-andross`, { cwd: tempDir, stdio: 'inherit' })
   // check the contents of ignite/ignite.json
-  const igniteJSON = jetpack.read(`${appDir}/ignite/ignite.json`)
+  const igniteJSON = filesystem.read(`${appDir}/ignite/ignite.json`)
   expect(typeof igniteJSON).toBe('string')
   expect(igniteJSON).toMatch(/"generators": {/)
 
   // check the Containers/App.js file
-  const appJS = jetpack.read(`${appDir}/App/Containers/App.js`)
+  const appJS = filesystem.read(`${appDir}/App/Containers/App.js`)
   expect(appJS).toMatch(/class App extends Component {/)
 
   // run ignite g component
-  await system.spawn(`cd ${appDir} && ${IGNITE} g component Test`)
-  expect(jetpack.inspect('App/Components/Test.js').type).toBe('file')
+  await system.spawn(`${IGNITE} g component Test`, opts)
+  expect(filesystem.inspect(`${appDir}/App/Components/Test.js`).type).toBe('file')
 
   // spork a screen and edit it
-  await system.spawn(`cd ${appDir} && ${IGNITE} spork component.ejs`)
+  await system.spawn(`${IGNITE} spork component.ejs`, opts)
   const sporkedFile = `${appDir}/ignite/Spork/ignite-ir-boilerplate-andross/component.ejs`
-  await jetpack.write(sporkedFile, 'SPORKED!')
-  expect(jetpack.inspect(sporkedFile).type).toBe('file')
-  await system.spawn(`cd ${appDir} && ${IGNITE} generate component Sporkified`)
-  expect(jetpack.read(`${appDir}/App/Components/Sporkified.js`)).toBe('SPORKED!')
+  await filesystem.write(sporkedFile, 'SPORKED!')
+  expect(filesystem.inspect(sporkedFile).type).toBe('file')
+  await system.spawn(`${IGNITE} generate component Sporkified`, opts)
+  expect(filesystem.read(`${appDir}/App/Components/Sporkified.js`)).toBe('SPORKED!')
 
   done()
 
