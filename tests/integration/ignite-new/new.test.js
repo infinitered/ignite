57a52039380212488168ccdc32eb921a390a73de
Jamon Holmgren <jamon@infinite.red>
2018-12-05 19:57:15 -0800

    Chown

--- tests/integration/ignite-new/new.test.js
+++ tests/integration/ignite-new/new.test.js
@@ -10,7 +10,7 @@ const originalDir = process.cwd()
 const opts = { stdio: 'inherit' }
 
 beforeAll(() => {
-  system.run(`alias ignite=${IGNITE}`)
+  // system.run(`alias ignite=${IGNITE}`)
 })
 
 beforeEach(() => {
