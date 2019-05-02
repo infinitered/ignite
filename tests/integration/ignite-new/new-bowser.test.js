28c558c02422e9542e4c546f0fc443fef12506ac
Jamon Holmgren <jamon@infinite.red>
2019-05-02 15:29:25 -0700

    chore(ci): Bump CI to refer to beta4 tag

--- tests/integration/ignite-new/new-bowser.test.js
+++ tests/integration/ignite-new/new-bowser.test.js
@@ -6,7 +6,7 @@ const IGNITE = 'node ' + filesystem.path(`${__dirname}/../../../bin/ignite`)
 
 // for local boilerplate testing
 // const IGNITE_BOILERPLATE = filesystem.path(__dirname, '..', '..', '..', '..', 'ignite-andross')
-const IGNITE_BOILERPLATE = 'ignite-bowser@4.0.0-beta.2'
+const IGNITE_BOILERPLATE = 'ignite-bowser@beta4'
 
 const APP_NAME = 'Foo'
 
