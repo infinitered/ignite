b2e515784af1df94254cd88c78ff83f23331e071
Morgan Donze <morganlaco@gmail.com>
2020-02-11 13:05:52 -0500

    Only list commands available in directory

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,43 @@
 import { GluegunToolbox } from 'gluegun'
+import { equals, replace } from 'ramda'
+import isIgniteDirectory from '../lib/is-ignite-directory'
+
+function commandAvailableInContext(command): boolean {
+  const isIgniteDir = isIgniteDirectory(process.cwd())
+
+  let availableCommands
+  if (isIgniteDir) {
+    availableCommands = ['add', 'doctor', 'generate', 'plugin', 'remove', 'spork', 'version']
+  } else {
+    availableCommands = ['doctor ', 'new', 'plugin', 'version']
+  }
+  return availableCommands.includes(command.name)
+}
+
+function printCommands(toolbox: GluegunToolbox, commandRoot?: string[]) {
+  const {
+    print: { info },
+  } = toolbox
+
+  const commandInfo = toolbox.runtime.commands.reduce((commands, command) => {
+    if (
+      !command.hidden &&
+      (!commandRoot || equals(command.commandPath.slice(0, commandRoot.length), commandRoot)) &&
+      commandAvailableInContext(command)
+    ) {
+      const alias = command.hasAlias() ? `(${command.aliases.join(', ')})` : ''
+      const commandPath = command.name ? command.commandPath.slice(0, -1).concat(command.name) : command.commandPath
+      commands.push([
+        `${commandPath.join(' ')} ${alias}`,
+        replace('$BRAND', toolbox.runtime.brand, command.description || '-'),
+      ])
+    }
+
+    return commands
+  }, [])
+  info(commandInfo)
+}
+
 module.exports = {
   dashed: true,
   alias: ['h'],
