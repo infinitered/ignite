567643baa0c72c4414878e87778ee76ed832c460
Bryan Stearns <bryanstearns@gmail.com>
2020-10-27 15:14:33 -0700

    Add a test for `ignite new --expo`

--- test/ignite-new.test.ts
+++ test/ignite-new.test.ts
@@ -3,6 +3,7 @@ import * as tempy from "tempy"
 import { run, runError } from "./_test-helpers"
 
 const APP_NAME = "Foo"
+const EXPO_APP_NAME = "Bar"
 
 const originalDir = process.cwd()
 let tempDir: string
@@ -35,6 +36,63 @@ test(`ignite new ${APP_NAME}`, async (done) => {
   const dirs = filesystem.list(`.`)
   expect(dirs).toContain("ios")
   expect(dirs).toContain("android")
+  expect(dirs).toContain("app")
+
+  // check the contents of ignite/templates
+  const templates = filesystem.list(`./ignite/templates`)
+  expect(templates).toContain("component")
+  expect(templates).toContain("model")
+  expect(templates).toContain("screen")
+
+  // check the basic contents of package.json
+  const igniteJSON = filesystem.read(`./package.json`, "json")
+  expect(igniteJSON).toHaveProperty("scripts")
+  expect(igniteJSON).toHaveProperty("dependencies")
+  expect(igniteJSON).toHaveProperty("detox.configurations")
+
+  // check the app.tsx file
+  const appJS = filesystem.read(`./app/app.tsx`)
+  expect(appJS).toContain("export default App")
+  expect(appJS).toContain("RootStore")
+
+  // now lets test generators too, since we have a properly spun-up app!
+  // components
+  const componentGen = await run(`generate component WompBomp`)
+  expect(componentGen).toContain(`app/components/womp-bomp/womp-bomp.tsx`)
+  expect(filesystem.list(`${process.cwd()}/app/components`)).toContain("womp-bomp")
+  expect(filesystem.read(`${process.cwd()}/app/components/womp-bomp/womp-bomp.tsx`)).toContain("export const WompBomp")
+
+  // models
+  const modelGen = await run(`generate model mod-test`)
+  expect(modelGen).toContain(`app/models/mod-test/mod-test.ts`)
+  expect(modelGen).toContain(`app/models/mod-test/mod-test.test.ts`)
+  expect(filesystem.list(`${process.cwd()}/app/models`)).toContain("mod-test")
+  expect(filesystem.read(`${process.cwd()}/app/models/mod-test/mod-test.ts`)).toContain("export const ModTestModel")
+
+  // screens
+  const screenGen = await run(`generate screen bowser-screen`)
+  expect(screenGen).toContain(`Stripping Screen from end of name`)
+  expect(screenGen).toContain(`app/screens/bowser/bowser-screen.tsx`)
+  expect(filesystem.list(`${process.cwd()}/app/screens/bowser`)).toContain("bowser-screen.tsx")
+  expect(filesystem.read(`${process.cwd()}/app/screens/bowser/bowser-screen.tsx`)).toContain(
+    "export const BowserScreen",
+  )
+
+  // we're done!
+  process.chdir("..")
+  done()
+})
+
+test(`ignite new ${EXPO_APP_NAME} --expo`, async (done) => {
+  const result = await run(`new ${EXPO_APP_NAME} --expo`)
+
+  expect(result).toContain(`Using expo-cli`)
+  expect(result).toContain(`Ignite CLI ignited ${EXPO_APP_NAME}`)
+
+  // now let's examine the spun-up app
+  process.chdir(EXPO_APP_NAME)
+
+  const dirs = filesystem.list(`.`)
   expect(dirs).toContain("app")
 
   // check the contents of ignite/templates
