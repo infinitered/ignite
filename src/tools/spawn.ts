3e08e747bb40355afff67abb159e11bf273fb566
Bryan Stearns <bryanstearns@gmail.com>
2020-10-27 14:56:11 -0700

    Make spawnProgress reject on nonzero exit code (and collect error output)

--- src/tools/spawn.ts
+++ src/tools/spawn.ts
@@ -5,9 +5,11 @@ export function spawnProgress(commandLine: string, options: SpawnOptions): Promi
   return new Promise((resolve, reject) => {
     const args = commandLine.split(" ")
     const spawned = require("cross-spawn")(args.shift(), args, options)
+    const errorOut = []
 
     spawned.stdout.on("data", (data) => options.onProgress && options.onProgress(data.toString()))
-    spawned.on("close", () => resolve())
+    spawned.stderr.on("data", (data) => errorOut.push(data))
+    spawned.on("close", (code) => (code === 0 ? resolve() : reject(errorOut.join())))
     spawned.on("error", (err) => reject(err))
   })
 }
