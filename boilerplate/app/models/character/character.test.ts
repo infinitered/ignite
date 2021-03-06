d7d40fe4eae6cb442a5e21b342783ea85a8b5b0e
Kate Kim <kateinkim@gmail.com>
2021-03-05 17:32:55 -0700

    fix character model test

--- boilerplate/app/models/character/character.test.ts
+++ boilerplate/app/models/character/character.test.ts
@@ -1,7 +1,10 @@
 import { CharacterModel } from "./character"
 
 test("can be created", () => {
-  const instance = CharacterModel.create({})
+  const instance = CharacterModel.create({
+    id: 1,
+    name: "Rick Sanchez"
+  })
 
   expect(instance).toBeTruthy()
 })
