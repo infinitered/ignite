a49a2228c60d2b69173f52f0e37d8e927572fee6
Niclas S <nirre79@gmail.com>
2021-04-11 00:31:21 +0200

    feat(boilerplate): Upgrade to React Native 0.64.x (#1641 by @nirre7)

--- boilerplate/ios/HelloWorld/AppDelegate.m
+++ boilerplate/ios/HelloWorld/AppDelegate.m
@@ -49,7 +49,11 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"HelloWorld"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+    if (@available(iOS 13.0, *)) {
+        rootView.backgroundColor = [UIColor systemBackgroundColor];
+    } else {
+        rootView.backgroundColor = [UIColor whiteColor];
+    }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
@@ -65,7 +69,7 @@ static void InitializeFlipper(UIApplication *application) {
     // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
     return extraModules;
 }
- 
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
