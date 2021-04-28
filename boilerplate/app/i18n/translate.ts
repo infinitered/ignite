7e11b1c5ff0e6550a75bdf483be919d308e7ccca
Shabith Ishan Thennakone <shabith@smashydesign.com>
2021-04-29 03:00:08 +0530

    fix(i18n): Fix small typo I18n -> i18n (#1668 by @shabith)

--- boilerplate/app/i18n/translate.ts
+++ boilerplate/app/i18n/translate.ts
@@ -6,6 +6,6 @@ import { TxKeyPath } from "./i18n"
  *
  * @param key The i18n key.
  */
-export function translate(key: TxKeyPath, options?: I18n.TranslateOptions) {
+export function translate(key: TxKeyPath, options?: i18n.TranslateOptions) {
   return key ? i18n.t(key, options) : null
 }
