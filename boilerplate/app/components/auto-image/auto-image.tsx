55f6ce200c1cacc1315e86e337860c2f98958edc
Mark Rickert <mjar81@gmail.com>
2021-06-25 13:00:52 -0400

    chore(docs): Adds comment for AutoImage (#1694 by @markrickert)
    [skip ci]

--- boilerplate/app/components/auto-image/auto-image.tsx
+++ boilerplate/app/components/auto-image/auto-image.tsx
@@ -10,6 +10,19 @@ type ImageProps = DefaultImageProps & {
   source: ImageURISource
 }
 
+/**
+ * An Image wrapper component that autosizes itself to the size of the actual image.
+ * You can always override by passing a width and height in the style.
+ * If passing only one of width/height this image component will use the actual
+ * size of the other dimension.
+ *
+ * This component isn't required, but is provided as a convenience so that
+ * we don't have to remember to explicitly set image sizes on every image instance.
+ *
+ * To use as a stand-in replacement import { AutoImage as Image } and remove the
+ * Image import from react-native. Now all images in that file are handled by this
+ * component and are web-ready if not explicitly sized in the style property.
+ */
 export function AutoImage(props: ImageProps) {
   const [imageSize, setImageSize] = useState({ width: 0, height: 0 })
 
