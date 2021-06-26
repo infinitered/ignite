99c5e4fa0c59a6d502bdcb92cb56bbe8f396970c
Jeroen Goddijn <48567344+JeroenGoddijn@users.noreply.github.com>
2021-06-25 22:48:38 -0500

    Update root-navigator.tsx
    Moved essential elements from main-navigator to root-navigator, so main-navigator can be safely removed.

--- boilerplate/app/navigators/root-navigator.tsx
+++ boilerplate/app/navigators/root-navigator.tsx
@@ -7,40 +7,41 @@
 import React from "react"
 import { NavigationContainer, NavigationContainerRef } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
-import { MainNavigator } from "./main-navigator"
-import { color } from "../theme"
+import { WelcomeScreen, DemoScreen, DemoListScreen } from "../screens"
 
 /**
  * This type allows TypeScript to know what routes are defined in this navigator
  * as well as what properties (if any) they might take when navigating to them.
  *
- * We recommend using MobX-State-Tree store(s) to handle state rather than navigation params.
+ * If no params are allowed, pass through `undefined`. Generally speaking, we
+ * recommend using your MobX-State-Tree store(s) to keep application state
+ * rather than passing state through navigation params.
  *
+
  * For more information, see this documentation:
  *   https://reactnavigation.org/docs/params/
  *   https://reactnavigation.org/docs/typescript#type-checking-the-navigator
  */
 export type RootParamList = {
-  mainStack: undefined
+  welcome: undefined
+  demo: undefined
+  demoList: undefined
 }
 
+// Documentation: https://reactnavigation.org/docs/stack-navigator/
 const Stack = createStackNavigator<RootParamList>()
 
 const RootStack = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        cardStyle: { backgroundColor: color.palette.deepPurple },
         headerShown: false,
       }}
+      initialRouteName="welcome"
     >
-      <Stack.Screen
-        name="mainStack"
-        component={MainNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
+      <Stack.Screen name="welcome" component={WelcomeScreen} />
+      <Stack.Screen name="demo" component={DemoScreen} />
+      <Stack.Screen name="demoList" component={DemoListScreen} />
     </Stack.Navigator>
   )
 }
@@ -57,3 +58,15 @@ export const RootNavigator = React.forwardRef<
 })
 
 RootNavigator.displayName = "RootNavigator"
+
+/**
+ * A list of routes from which we're allowed to leave the app when
+ * the user presses the back button on Android.
+ *
+ * Anything not on this list will be a standard `back` action in
+ * react-navigation.
+ *
+ * `canExit` is used in ./app/app.tsx in the `useBackButtonHandler` hook.
+ */
+const exitRoutes = ["welcome"]
+export const canExit = (routeName: string) => exitRoutes.includes(routeName)
