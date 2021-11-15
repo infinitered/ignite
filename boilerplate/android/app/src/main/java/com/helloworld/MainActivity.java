5e72518f5f1e23e8eda365d3c8dd8d9ff8ee5a1f
Leon Kim <minusnine@gmail.com>
2021-11-16 08:47:31 +0900

    build(deps): android configurations for expo modules

--- boilerplate/android/app/src/main/java/com/helloworld/MainActivity.java
+++ boilerplate/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
+import expo.modules.ReactActivityDelegateWrapper;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -23,11 +25,14 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+    return new ReactActivityDelegateWrapper(
+      this,
+      new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
       }
-    };
+    );
   }
 }
