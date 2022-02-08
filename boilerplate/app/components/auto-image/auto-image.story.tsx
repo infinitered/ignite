410404d9a98b40cd7bdb42950ec8b1f8199ee8b4
Robin Heinze <robin.m.heinze@gmail.com>
2022-02-08 15:49:19 -0800

    Use different image for auto image story

--- boilerplate/app/components/auto-image/auto-image.story.tsx
+++ boilerplate/app/components/auto-image/auto-image.story.tsx
@@ -6,7 +6,7 @@ import { AutoImage } from "./auto-image"
 
 declare let module
 
-const bowser = require("../../screens/welcome/bowser.png")
+const rick = require("./rick.png")
 const morty = { uri: "https://rickandmortyapi.com/api/character/avatar/2.jpeg" }
 
 storiesOf("AutoImage", module)
@@ -14,11 +14,11 @@ storiesOf("AutoImage", module)
   .add("Style Presets", () => (
     <Story>
       <UseCase text="With require()">
-        <AutoImage source={bowser} />
-        <AutoImage source={bowser} style={{ width: 150 }} />
-        <AutoImage source={bowser} style={{ width: 150, height: 150 }} />
-        <AutoImage source={bowser} style={{ height: 150 }} />
-        <AutoImage source={bowser} style={{ height: 150, resizeMode: "contain" }} />
+        <AutoImage source={rick} />
+        <AutoImage source={rick} style={{ width: 150 }} />
+        <AutoImage source={rick} style={{ width: 150, height: 150 }} />
+        <AutoImage source={rick} style={{ height: 150 }} />
+        <AutoImage source={rick} style={{ height: 150, resizeMode: "contain" }} />
       </UseCase>
       <UseCase text="With URL">
         <AutoImage source={morty} />
