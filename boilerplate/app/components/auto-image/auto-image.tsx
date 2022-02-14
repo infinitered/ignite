c47bd28f23a24432d85d44ace63c014f54537c2f
Jamon Holmgren <jamonholmgren@gmail.com>
2022-02-14 12:14:36 -0800

    Fixes AsyncImage bug when component unmounts

--- boilerplate/app/components/auto-image/auto-image.tsx
+++ boilerplate/app/components/auto-image/auto-image.tsx
@@ -27,20 +27,26 @@ export function AutoImage(props: ImageProps) {
   const [imageSize, setImageSize] = useState({ width: 0, height: 0 })
 
   useLayoutEffect(() => {
+    let mounted = true
+
     if (props.source?.uri) {
       RNImage.getSize(props.source.uri as any, (width, height) => {
-        setImageSize({ width, height })
+        if (mounted) setImageSize({ width, height })
       })
     } else if (Platform.OS === "web") {
       // web requires a different method to get it's size
       RNImage.getSize(props.source as any, (width, height) => {
-        setImageSize({ width, height })
+        if (mounted) setImageSize({ width, height })
       })
     } else {
       const { width, height } = RNImage.resolveAssetSource(props.source)
       setImageSize({ width, height })
     }
-  }, [])
+
+    return () => {
+      mounted = false
+    }
+  }, [props.source])
 
   return <RNImage {...props} style={[imageSize, props.style]} />
 }
