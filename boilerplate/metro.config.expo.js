f826586b4e282ef7265ced621b6b5d76aaf9b92e
Jamon Holmgren <jamonholmgren@gmail.com>
2022-02-28 12:07:07 -0800

    Add comment [skip ci]

--- boilerplate/metro.config.expo.js
+++ boilerplate/metro.config.expo.js
@@ -2,3 +2,6 @@
 const { getDefaultConfig } = require("expo/metro-config")
 
 module.exports = getDefaultConfig(__dirname)
+
+// For one idea on how to support symlinks in Expo, see:
+// https://github.com/infinitered/ignite/issues/1904#issuecomment-1054535068
