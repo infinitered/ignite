9d1c648da438079f2ff05963c2c0d42d3d2786e2
Jamon Holmgren <jamonholmgren@gmail.com>
2022-06-06 11:40:06 -0700

    Disables symlink support

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -8,15 +8,28 @@
  * However, it doesn't hurt to have it either.
  */
 const { makeMetroConfig } = require("@rnx-kit/metro-config")
-const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
+// const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
 const { getDefaultConfig } = require("metro-config")
 
 module.exports = (async () => {
   const defaultConfig = await getDefaultConfig()
   return makeMetroConfig({
     projectRoot: __dirname,
+    // watchFolders: [`${__dirname}/../..`], // for monorepos
     resolver: {
-      resolveRequest: MetroSymlinksResolver(),
+      /**
+       * This custom resolver is for if you're using symlinks.
+       * It's disabled because it results in an error for now, specifically:
+       *
+       * error: Error: Unable to resolve module PROJECTDIR/node_modules/metro-runtime/src/modules/empty-module.js
+       * from PROJECTDIR/_: PROJECTDIR/node_modules/metro-runtime/src/modules/empty-module.js could not be found
+       *
+       * This could be a bug in @rnx-kit/metro-resolver-symlinks.
+       *
+       * Issue over there: https://github.com/microsoft/rnx-kit/issues/1605
+       *
+       */
+      // resolveRequest: MetroSymlinksResolver(),
       assetExts: [...defaultConfig.resolver.assetExts, "bin"],
     },
   })
