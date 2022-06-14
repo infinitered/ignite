2435d395297a624d69d0781d197790aca317a8cf
Jamon Holmgren <jamonholmgren@gmail.com>
2022-06-14 09:15:40 -0700

    Added warning to rename CLI command

--- src/commands/rename.ts
+++ src/commands/rename.ts
@@ -1,12 +1,14 @@
 import { GluegunToolbox } from "gluegun"
-import { warning } from "../tools/pretty"
+import { direction, heading, p, warning } from "../tools/pretty"
 import { renameReactNativeApp } from "../tools/react-native"
 
 module.exports = {
   alias: ["rn"],
   description: "Renames a React Native and/or Ignite app",
   run: async (toolbox: GluegunToolbox) => {
-    const { parameters, prompt, filesystem } = toolbox
+    const { parameters, prompt, filesystem, print } = toolbox
+    const { colors, info } = print
+    const { red, green } = colors
 
     let newName = parameters.first
     let newBundleIdentifier = parameters.options.bundle
@@ -77,5 +79,18 @@ module.exports = {
     }
 
     await renameReactNativeApp(toolbox, oldName, newName, oldBundleIdentifier, newBundleIdentifier)
+
+    heading(`Ignite successfully renamed your app from ${red(oldName)} to ${green(newName)}!`)
+    p()
+    heading(`Caveats:`)
+    p()
+    info(`    * Ignite's rename feature is not 100% perfect in all cases.`)
+    info(`      Carefully examine the diff it produces with \`git diff\` before committing.`)
+    info(`    * Additionally, you'll want to re-run \`pod install\` and`)
+    info(`      rebuild your app and caches. We don't make any changes to those files or folders.`)
+    p()
+    direction(`Next: run \`git diff\` to see the changes we made.`)
+    direction(`To reset everything: run \`git reset --hard && git clean -fd\``)
+    warning(`(${red("careful")}: this will remove all other changes you haven't committed!)`)
   },
 }
