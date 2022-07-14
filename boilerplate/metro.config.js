40f582ed3fb2b416b1d4f392dbb69551318fe851
Jamon Holmgren <jamonholmgren@gmail.com>
2022-07-14 14:14:43 -0700

    Initial attempt to implement @rnx-kit/metro-serializer-esbuild

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -6,16 +6,19 @@
  * using PNPM or monorepo or symlinks at all.
  *
  * However, it doesn't hurt to have it either.
+ *
+ * We've also added metro-serializer-esbuild so that we can get tree shaking.
  */
 const { makeMetroConfig } = require("@rnx-kit/metro-config")
 const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
 const { getDefaultConfig } = require("metro-config")
+const { MetroSerializer, esbuildTransformerConfig } = require("@rnx-kit/metro-serializer-esbuild")
 
 module.exports = (async () => {
   const defaultConfig = await getDefaultConfig()
   return makeMetroConfig({
     projectRoot: __dirname,
-    // watchFolders: [`${__dirname}/../..`], // for monorepos
+    // watchFolders: [`${__dirname}/../..`], // enable for monorepos
     resolver: {
       /**
        * This custom resolver is for if you're using symlinks.
@@ -25,5 +28,9 @@ module.exports = (async () => {
       resolveRequest: MetroSymlinksResolver(),
       assetExts: [...defaultConfig.resolver.assetExts, "bin"],
     },
+    // esbuild custom serializer for speed and tree shaking
+    // Note the caveats here: https://github.com/microsoft/rnx-kit/tree/main/packages/metro-serializer-esbuild#known-limitations
+    serializer: { customSerializer: MetroSerializer() },
+    transformer: esbuildTransformerConfig,
   })
 })()
