2b6a59b3ec9dc8601f551ed85ae1ca3d1595a4c3
Yulian Glukhenko <yulian@infinite.red>
2022-07-20 11:14:27 -0700

    Adds expo-fonts library and installs SpaceGrotesk font.

--- boilerplate/react-native.config.js
+++ boilerplate/react-native.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  assets: ["./assets/fonts/"],
+  assets: [
+    // If you need to add non-google fonts (those not available through the `@expo-google-fonts/**`
+    // packages, you can add them to the referenced path below and uncomment this line.
+    // "./assets/fonts/"
+  ],
 }
