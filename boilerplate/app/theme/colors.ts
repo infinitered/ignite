5534b41aacb07d3bb14976123f20d6b7a5112aad
Yulian Glukhenko <yulian@infinite.red>
2022-07-20 11:26:54 -0700

    Removes notes from colors.ts.

--- boilerplate/app/theme/colors.ts
+++ boilerplate/app/theme/colors.ts
@@ -1,14 +1,5 @@
 import { palette } from "./palette"
 
-/**
- * Roles for colors.  Prefer using these over the palette.  It makes it easier
- * to change things.
- *
- * The only roles we need to place in here are the ones that span through the app.
- *
- * If you have a specific use-case, like a spinner color.  It makes more sense to
- * put that in the <Spinner /> component.
- */
 export const colors = {
   /**
    * The palette is available to use, but prefer using the name.
