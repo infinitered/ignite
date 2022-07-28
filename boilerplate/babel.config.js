bf564ebdb1e53f60a8e42ad83f267402c0039486
Frank Calise <fcalise@gmail.com>
2022-07-28 00:29:22 -0700

    fix(boilerplate): unified metro and babel configs

--- boilerplate/babel.config.js
+++ boilerplate/babel.config.js
@@ -1,3 +1,35 @@
+const vanillaConfig = {
+  presets: ["module:metro-react-native-babel-preset"],
+  env: {
+    production: {},
+  },
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    ["@babel/plugin-proposal-optional-catch-binding"],
+  ],
+}
+
+const expoConfig = {
+  presets: ["babel-preset-expo"],
+  env: {
+    production: {},
+  },
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    ["@babel/plugin-proposal-optional-catch-binding"],
+  ],
+}
+
 let isExpo = false
 try {
   const Constants = require("expo-constants")
@@ -7,6 +39,6 @@ try {
     Constants.executionEnvironment === "storeClient"
 } catch {}
 
-const babelConfig = isExpo ? require("./babel.config.expo") : require("./babel.config.vanilla")
+const babelConfig = isExpo ? expoConfig : vanillaConfig
 
 module.exports = babelConfig
--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -1,3 +1,4 @@
+let metroConfig
 let isExpo = false
 try {
   const Constants = require("expo-constants")
@@ -7,6 +8,44 @@ try {
     Constants.executionEnvironment === "storeClient"
 } catch {}
 
-const metroConfig = isExpo ? require("./metro.config.expo") : require("./metro.config.vanilla")
+if (isExpo) {
+  /**
+   *  Expo metro config
+   * Learn more https://docs.expo.io/guides/customizing-metro
+
+   * For one idea on how to support symlinks in Expo, see:
+   * https://github.com/infinitered/ignite/issues/1904#issuecomment-1054535068
+   */
+} else {
+  /**
+   * Vanilla metro config - we're using a custom metro config because we want to support symlinks
+   * out of the box. This allows you to use pnpm and/or play better in a monorepo.
+   *
+   * You can safely delete this file and remove @rnx-kit/metro-* if you're not
+   * using PNPM or monorepo or symlinks at all.
+   *
+   * However, it doesn't hurt to have it either.
+   */
+  const { makeMetroConfig } = require("@rnx-kit/metro-config")
+  const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
+  const { getDefaultConfig } = require("metro-config")
+
+  metroConfig = (async () => {
+    const defaultConfig = await getDefaultConfig()
+    return makeMetroConfig({
+      projectRoot: __dirname,
+      // watchFolders: [`${__dirname}/../..`], // for monorepos
+      resolver: {
+        /**
+         * This custom resolver is for if you're using symlinks.
+         *
+         * You can disable it if you're not using pnpm or a monorepo or symlinks.
+         */
+        resolveRequest: MetroSymlinksResolver(),
+        assetExts: [...defaultConfig.resolver.assetExts, "bin"],
+      },
+    })
+  })()
+}
 
 module.exports = metroConfig
