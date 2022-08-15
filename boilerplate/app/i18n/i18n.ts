5e4e76b12281d3b57e9602de54c05b5a6c1f58e8
Mazen Chami <mazenchami@gmail.com>
2022-08-15 16:44:32 -0400

    making sure we capture all possible localization translations

--- boilerplate/app/i18n/i18n.ts
+++ boilerplate/app/i18n/i18n.ts
@@ -4,9 +4,14 @@ import en from "./en.json"
 import ja from "./ja.json"
 
 i18n.fallbacks = true
-i18n.translations = { en, ja }
+/**
+ * we need always include "*-US" for some valid language codes because when you change the system language,
+ * the language code is the suffixed with "-US". i.e. if a device is set to English ("en"),
+ * if you change to another language and then return to English language code is now "en-US".
+ */
+i18n.translations = { en, "en-US": en, ja }
 
-i18n.locale = Localization.locale || "en"
+i18n.locale = Localization.locale
 
 /**
  * Builds up valid keypaths for translations.
