9ec2bb784189472c39e873d04ff2f23faf776469
Jamon Holmgren <jamonholmgren@gmail.com>
2022-08-15 11:05:34 -0700

    Moving back to original for this PR

--- boilerplate/app/navigators/app-navigator.tsx
+++ boilerplate/app/navigators/app-navigator.tsx
@@ -51,7 +51,7 @@ const AppStack = () => {
 
 interface NavigationProps extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
 
-export const AppNavigator = (props: NavigationProps) => {
+export function AppNavigator(props: NavigationProps) {
   const colorScheme = useColorScheme()
 
   // What route names do we allow the back button to exit the app from?
@@ -70,3 +70,15 @@ export const AppNavigator = (props: NavigationProps) => {
 }
 
 AppNavigator.displayName = "AppNavigator"
+
+/**
+ * A list of routes from which we're allowed to leave the app when
+ * the user presses the back button on Android.
+ *
+ * Anything not on this list will be a standard `back` action in
+ * react-navigation.
+ *
+ * `canExit` is used in ./app/app.tsx in the `useBackButtonHandler` hook.
+ */
+const exitRoutes = ["Welcome"]
+export const canExit = (routeName: string) => exitRoutes.includes(routeName)
