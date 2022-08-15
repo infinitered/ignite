f391f4908404f99872f79bf9413cedd16b5334f0
Frank Calise <fcalise@gmail.com>
2022-08-15 19:34:57 -0400

    fix(vendor): update react-native-bootsplash to 4.3.2

--- boilerplate/android/app/src/main/java/com/helloworld/MainActivity.java
+++ boilerplate/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,5 +1,7 @@
 package com.helloworld;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -28,6 +30,12 @@ public class MainActivity extends ReactActivity {
     return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);            // <- initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+  }
+
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
@@ -46,12 +54,6 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-    }
-
-    @Override
-    protected void loadApp(String appKey) {
-      RNBootSplash.init(getPlainActivity());
-      super.loadApp(appKey);
     }
   }
 }
