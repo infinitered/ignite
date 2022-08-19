22118e12887c562c1fd528ab63ee6895586594ce
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-08-19 15:10:16 -0700

    fix(ignite-new.test): add --y flag

--- test/vanilla/ignite-new.test.ts
+++ test/vanilla/ignite-new.test.ts
@@ -29,7 +29,7 @@ describe("Checking for ignite. 🪔", () => {
 
 describe("Igniting new app! 🔥\nGo get a coffee or something. This is gonna take a while.", () => {
   test(`ignite new ${APP_NAME}`, async () => {
-    const result = await runIgnite(`new ${APP_NAME} --debug --packager=npm`, {
+    const result = await runIgnite(`new ${APP_NAME} --debug --packager=npm --y`, {
       pre: `cd ${tempDir}`,
       post: `cd ${originalDir}`,
     })
