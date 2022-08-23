7f1dcbdd52b12a8a1265ceaf430816aa5b366cb0
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-08-23 10:36:03 -0700

    refactor(remove-demo): use patching.exists, re-order file

--- src/commands/remove-demo.ts
+++ src/commands/remove-demo.ts
@@ -6,29 +6,23 @@ module.exports = {
   alias: ["rd", "remove-demos"],
   description: "Remove demo code from generated boilerplate",
   run: async (toolbox: GluegunToolbox) => {
-    enum DemoComment {
-      REMOVE_FILE = `// @demo remove-file`,
-    }
-
-    p()
-
-    const { parameters, filesystem } = toolbox
+    const { parameters, patching, filesystem } = toolbox
 
     const CWD = process.cwd()
     const TARGET_DIR = parameters.first ?? CWD
-
+    p()
     p(`Removing demo code from ${TARGET_DIR}`)
 
     const getAllFilePaths = createGetAllFilePaths(filesystem)
-
     const paths = getAllFilePaths(TARGET_DIR)
-
     p(`Found ${paths.length} files`)
 
+    enum DemoComment {
+      REMOVE_FILE = `// @demo remove-file`,
+    }
+    // Go through every file path and handle the demo comment in each file
     for (const path of paths) {
-      const contents = filesystem.read(path)
-
-      if (contents.includes(DemoComment.REMOVE_FILE)) {
+      if (await patching.exists(path, DemoComment.REMOVE_FILE)) {
         filesystem.remove(path)
         p(`Removed ${path}`)
       }
