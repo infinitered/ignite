0f0ab47ac0af0fd2c9efb60fb0d26b6d5c08eb4f
Simran Kaur <simran1315@gmail.com>
2022-08-25 10:24:44 -0400

    add comments to snacked reactotron/index

--- boilerplate/bin/snackify.sh
+++ boilerplate/bin/snackify.sh
@@ -12,7 +12,10 @@ then
     rm -rf ./app/services/reactotron
     mkdir ./app/services/reactotron && cd ./app/services/reactotron
     touch index.ts
-    echo "export const setupReactotron = (arg1:any) => null
+    echo "/**
+ * This file is loaded for snack to resolve Reactotron for expo snack
+ **/
+    export const setupReactotron = (arg1:any) => null
     export const setReactotronRootStore=(arg1:any, arg2:any)=>null" > index.ts
     yarn add expo-constants@~13.2.4 expo-localization@~13.1.0 expo-modules-core@~0.11.4 expo-splash-screen@~0.16.2 react-native-screens@~3.15.0 @react-native-async-storage/async-storage@~1.17.3
     yarn remove @expo/webpack-config reactotron-react-native detox detox-expo-helpers ts-jest jest metro-config @rnx-kit/metro-config reactotron-mst reactotron-react-js reactotron-core-client
