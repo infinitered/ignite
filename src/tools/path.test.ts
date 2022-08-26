fa70b50da6adeba7260f75fdb11f16ef9b026054
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2022-08-26 13:05:13 -0700

    fix(path.test): update snapshot not to include homedir (#2070)

--- src/tools/path.test.ts
+++ src/tools/path.test.ts
@@ -3,7 +3,7 @@ import { BOILERPLATE_PATH, createGetAllFilePaths } from "./path"
 
 describe("paths", () => {
   describe("getAllFilePaths", () => {
-    it(`should return expected paths in '${BOILERPLATE_PATH}'`, () => {
+    it(`should return expected paths in '~/boilerplate'`, () => {
       const getAllFilePaths = createGetAllFilePaths(filesystem)
       const paths = getAllFilePaths(BOILERPLATE_PATH).map((path) => path.replace(process.cwd(), ""))
 
