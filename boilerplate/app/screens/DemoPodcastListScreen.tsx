9a63ed6683897af42757ba708e2a93e159fe4768
Kate Kim <kateinkim@gmail.com>
2022-08-29 11:12:44 -0600

    fix initial list render issue

--- boilerplate/app/screens/DemoPodcastListScreen.tsx
+++ boilerplate/app/screens/DemoPodcastListScreen.tsx
@@ -40,7 +40,7 @@ export const DemoPodcastListScreen = observer(function DemoPodcastListScreen(
     <Screen preset="fixed" safeAreaEdges={["top"]}>
       <FlatList<Episode>
         data={episodeStore.episodesForList}
-        extraData={episodeStore.favorites.length}
+        extraData={episodeStore.episodeForList.length}
         contentContainerStyle={$flatListContentContainer}
         refreshing={refreshing}
         onRefresh={manualRefresh}
