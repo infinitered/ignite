b9006e677c0f0b50f6dcae521fbbf1ebfea15ba0
Simran Kaur <simran1315@gmail.com>
2022-09-07 17:47:50 -0400

    add snackify to ignite-cli

--- src/commands/snackify.ts
+++ src/commands/snackify.ts
@@ -9,10 +9,12 @@ module.exports = {
     const { remove } = filesystem
     const { info } = print
 
+    // getStatus checks if there are any uncommitted changes inside the branch
     const getStatus = await system.run(`git status -s`)
-    if (getStatus === "" || true) {
-      // test if the branch name is snack- something
-      const branchName = await system.run(`git name-rev --name-only HEAD`)
+
+    if (getStatus === "") {
+      // test if the branch name has substring snack
+      const branchName = await system.run(`git branch --show-current`)
       if (!branchName.includes("snack")) {
         info("Creating a new branch for your snack")
         system.run(`git checkout -b snack-$(date +'%m%d%y')`)
@@ -21,6 +23,7 @@ module.exports = {
       info(
         "There are uncommitted changes in your current branch, please commit changes before running this",
       )
+      return
     }
 
     info("This command will create an expo snack project for you. ")
@@ -30,7 +33,8 @@ module.exports = {
       "- /ios\n- /android\n- /e2e\n- /test\n- /app/services/reactotron\n- jest.config.js\n- metro.config.js\n- webpack.config.js",
     )
 
-    const test = await prompt.confirm("Do you wish to continue", true)
+    const confirm = await prompt.confirm("Do you wish to continue", true)
+    // the following array contains folders and files that are deleted to make project ready for some snacking
     const removeArray = [
       "./ios",
       "./android",
@@ -41,38 +45,41 @@ module.exports = {
       "./webpack.config.js",
       "./app/services/reactotron",
     ]
-    const addPackages = [
-      "expo-constants@~13.2.4",
-      "expo-localization@~13.1.0",
-      "expo-modules-core@~0.11.4",
-      "expo-splash-screen@~0.16.2",
-      "react-native-screens@~3.15.0",
-      "@react-native-async-storage/async-storage@~1.17.3",
-    ]
-    const removePackages = [
-      "@expo/webpack-config",
-      "reactotron-react-native",
-      "detox",
-      "detox-expo-helpers",
-      "ts-jest",
-      "jest",
-      "metro-config",
-      "@rnx-kit/metro-config",
-      "reactotron-mst",
-      "reactotron-react-js",
-      "reactotron-core-client",
-    ]
-    if (test) {
+
+    if (confirm) {
+      // 1. delete all folders and files that are not required
       removeArray.map((val) => remove(val))
+      // 2. Create a new file to handle reactotron
       filesystem.file("./app/services/reactotron/index.ts")
       patching.replace(
         "./app/services/reactotron/index.ts",
         "",
         "/*** This file is loaded for snack to resolve Reactotron for expo snack**/ \n export const setupReactotron = (arg1:any) => null \nexport const setReactotronRootStore=(arg1:any, arg2:any)=>null",
       )
-      addPackages.map((val) => packager.add(val))
-      removePackages.map((val) => packager.remove(val))
+      // 3. Update/Add packages as per expo snack's requirements
+      await packager.add("expo-constants@~13.2.4")
+      await packager.add("expo-localization@~13.1.0")
+      await packager.add("expo-modules-core@~0.11.4")
+      await packager.add("expo-splash-screen@~0.16.2")
+      await packager.add("react-native-screens@~3.15.0")
+      await packager.add("@react-native-async-storage/async-storage@~1.17.3")
+
+      // 4. Remove packages that are not required
+      await packager.remove("@expo/webpack-config")
+      await packager.remove("reactotron-react-native")
+      await packager.remove("detox")
+      await packager.remove("detox-expo-helpers")
+      await packager.remove("ts-jest")
+      await packager.remove("jest")
+      await packager.remove("metro-config")
+      await packager.remove("@rnx-kit/metro-config")
+      await packager.remove("reactotron-mst")
+      await packager.remove("reactotron-react-js")
+      await packager.remove("reactotron-core-client")
     }
-    info("All done")
+    info("All done, your app is ready to be imported into expo snack")
+    info(
+      "The best way to import this project into an expo snack is via publishing this project into a public repository on github and then adding your repo's URL to an expo snack",
+    )
   },
 }
