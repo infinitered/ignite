0d1aab71840d414e671505f2a312360fe51d6bc4
Arslan Dogar <38478270+arslandogar@users.noreply.github.com>
2022-09-09 05:13:55 +0500

    fix (#1942): Prevent trying to update the component if not mounted (#2106 by @arslandogar)

---
+++ boilerplate/app/utils/is-mounted.ts
@@ -0,0 +1,15 @@
+import { useEffect, useCallback, useRef } from "react"
+
+export function useIsMounted() {
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  return useCallback(() => isMounted.current, [])
+}
