3d14d061f50db5fe145e62f3d6603950d3bc6815
Yulian Glukhenko <yulian@infinite.red>
2022-11-16 15:57:31 -0800

    Updates DemoShowroomScreen to use useSafeAreaInsetsStyle hook.

--- boilerplate/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
+++ boilerplate/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
@@ -2,11 +2,11 @@ import React, { FC, ReactElement, useEffect, useRef, useState } from "react"
 import { FlatList, Image, ImageStyle, SectionList, TextStyle, View, ViewStyle } from "react-native"
 import { DrawerLayout, DrawerState } from "react-native-gesture-handler"
 import { useSharedValue } from "react-native-reanimated"
-import { SafeAreaView } from "react-native-safe-area-context"
 import { ListItem, Screen, Text } from "../../components"
 import { isRTL } from "../../i18n"
 import { DemoTabScreenProps } from "../../navigators/DemoNavigator"
 import { colors, spacing } from "../../theme"
+import { useSafeAreaInsetsStyle } from "../../utils/useSafeAreaInsetsStyle"
 import * as Demos from "./demos"
 import { DrawerIconButton } from "./DrawerIconButton"
 
@@ -20,141 +20,139 @@ export interface Demo {
 
 export const DemoShowroomScreen: FC<DemoTabScreenProps<"DemoShowroom">> =
   function DemoShowroomScreen(_props) {
-  const [open, setOpen] = useState(false)
-  const timeout = useRef<ReturnType<typeof setTimeout>>()
-  const drawerRef = useRef<DrawerLayout>()
-  const listRef = useRef<SectionList>()
-  const menuRef = useRef<FlatList>()
-  const progress = useSharedValue(0)
+    const [open, setOpen] = useState(false)
+    const timeout = useRef<ReturnType<typeof setTimeout>>()
+    const drawerRef = useRef<DrawerLayout>()
+    const listRef = useRef<SectionList>()
+    const menuRef = useRef<FlatList>()
+    const progress = useSharedValue(0)
 
-  const toggleDrawer = () => {
-    if (!open) {
-      setOpen(true)
-      drawerRef.current?.openDrawer({ speed: 2 })
-    } else {
-      setOpen(false)
-      drawerRef.current?.closeDrawer({ speed: 2 })
+    const toggleDrawer = () => {
+      if (!open) {
+        setOpen(true)
+        drawerRef.current?.openDrawer({ speed: 2 })
+      } else {
+        setOpen(false)
+        drawerRef.current?.closeDrawer({ speed: 2 })
+      }
     }
-  }
 
-  const handleScroll = (sectionIndex: number, itemIndex = 0) => {
-    listRef.current.scrollToLocation({
-      animated: true,
-      itemIndex,
-      sectionIndex,
-    })
-    toggleDrawer()
-  }
+    const handleScroll = (sectionIndex: number, itemIndex = 0) => {
+      listRef.current.scrollToLocation({
+        animated: true,
+        itemIndex,
+        sectionIndex,
+      })
+      toggleDrawer()
+    }
 
-  const scrollToIndexFailed = (info: {
-    index: number
-    highestMeasuredFrameIndex: number
-    averageItemLength: number
-  }) => {
-    listRef.current?.getScrollResponder()?.scrollToEnd()
-    timeout.current = setTimeout(
-      () =>
-        listRef.current?.scrollToLocation({
-          animated: true,
-          itemIndex: info.index,
-          sectionIndex: 0,
-        }),
-      50,
+    const scrollToIndexFailed = (info: {
+      index: number
+      highestMeasuredFrameIndex: number
+      averageItemLength: number
+    }) => {
+      listRef.current?.getScrollResponder()?.scrollToEnd()
+      timeout.current = setTimeout(
+        () =>
+          listRef.current?.scrollToLocation({
+            animated: true,
+            itemIndex: info.index,
+            sectionIndex: 0,
+          }),
+        50,
+      )
+    }
+
+    useEffect(() => {
+      return () => timeout.current && clearTimeout(timeout.current)
+    }, [])
+
+    const $drawerInsets = useSafeAreaInsetsStyle(["top"])
+
+    return (
+      <DrawerLayout
+        ref={drawerRef}
+        drawerWidth={326}
+        drawerType={"slide"}
+        drawerPosition={isRTL ? "right" : "left"}
+        drawerBackgroundColor={colors.palette.neutral100}
+        overlayColor={colors.palette.overlay20}
+        onDrawerSlide={(drawerProgress) => {
+          progress.value = open ? 1 - drawerProgress : drawerProgress
+        }}
+        onDrawerStateChanged={(newState: DrawerState, drawerWillShow: boolean) => {
+          if (newState === "Settling") {
+            setOpen(drawerWillShow)
+          }
+        }}
+        renderNavigationView={() => (
+          <View style={[$drawer, $drawerInsets]}>
+            <View style={$logoContainer}>
+              <Image source={logo} style={$logoImage} />
+            </View>
+
+            <FlatList<{ name: string; useCases: string[] }>
+              ref={menuRef}
+              contentContainerStyle={$flatListContentContainer}
+              data={Object.values(Demos).map((d) => ({
+                name: d.name,
+                useCases: d.data.map((u) => u.props.name),
+              }))}
+              keyExtractor={(item) => item.name}
+              renderItem={({ item, index: sectionIndex }) => (
+                <View>
+                  <Text
+                    onPress={() => handleScroll(sectionIndex)}
+                    preset="bold"
+                    style={$menuContainer}
+                  >
+                    {item.name}
+                  </Text>
+                  {item.useCases.map((u, index) => (
+                    <ListItem
+                      key={`section${sectionIndex}-${u}`}
+                      onPress={() => handleScroll(sectionIndex, index + 1)}
+                      text={u}
+                      rightIcon={isRTL ? "caretLeft" : "caretRight"}
+                    />
+                  ))}
+                </View>
+              )}
+            />
+          </View>
+        )}
+      >
+        <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
+          <DrawerIconButton onPress={toggleDrawer} {...{ open, progress }} />
+
+          <SectionList
+            ref={listRef}
+            contentContainerStyle={$sectionListContentContainer}
+            stickySectionHeadersEnabled={false}
+            sections={Object.values(Demos)}
+            renderItem={({ item }) => item}
+            renderSectionFooter={() => <View style={$demoUseCasesSpacer} />}
+            ListHeaderComponent={
+              <View style={$heading}>
+                <Text preset="heading" tx="demoShowroomScreen.jumpStart" />
+              </View>
+            }
+            onScrollToIndexFailed={scrollToIndexFailed}
+            renderSectionHeader={({ section }) => {
+              return (
+                <View>
+                  <Text preset="heading" style={$demoItemName}>
+                    {section.name}
+                  </Text>
+                  <Text style={$demoItemDescription}>{section.description}</Text>
+                </View>
+              )
+            }}
+          />
+        </Screen>
+      </DrawerLayout>
     )
   }
-
-  useEffect(() => {
-    return () => timeout.current && clearTimeout(timeout.current)
-  }, [])
-
-  return (
-    <DrawerLayout
-      ref={drawerRef}
-      drawerWidth={326}
-      drawerType={"slide"}
-      drawerPosition={isRTL ? "right" : "left"}
-      drawerBackgroundColor={colors.palette.neutral100}
-      overlayColor={colors.palette.overlay20}
-      onDrawerSlide={(drawerProgress) => {
-        progress.value = open ? 1 - drawerProgress : drawerProgress
-      }}
-      onDrawerStateChanged={(newState: DrawerState, drawerWillShow: boolean) => {
-        if (newState === "Settling") {
-          setOpen(drawerWillShow)
-        }
-      }}
-      renderNavigationView={() => (
-        <SafeAreaView style={$drawer} edges={["top"]}>
-          <View style={$logoContainer}>
-            <Image source={logo} style={$logoImage} />
-          </View>
-
-          <FlatList<{ name: string; useCases: string[] }>
-            ref={menuRef}
-            contentContainerStyle={$flatListContentContainer}
-            data={Object.values(Demos).map((d) => ({
-              name: d.name,
-              useCases: d.data.map((u) => u.props.name),
-            }))}
-            keyExtractor={(item) => item.name}
-            renderItem={({ item, index: sectionIndex }) => (
-              <View>
-                <Text
-                  onPress={() => handleScroll(sectionIndex)}
-                  preset="bold"
-                  style={$menuContainer}
-                >
-                  {item.name}
-                </Text>
-                {item.useCases.map((u, index) => (
-                  <ListItem
-                    key={`section${sectionIndex}-${u}`}
-                    onPress={() => handleScroll(sectionIndex, index + 1)}
-                    text={u}
-                    rightIcon={isRTL ? "caretLeft" : "caretRight"}
-                  />
-                ))}
-              </View>
-            )}
-          />
-        </SafeAreaView>
-      )}
-    >
-      <Screen
-        preset="fixed"
-        safeAreaEdges={["top", "bottom"]}
-        contentContainerStyle={$screenContainer}
-      >
-        <DrawerIconButton onPress={toggleDrawer} {...{ open, progress }} />
-
-        <SectionList
-          ref={listRef}
-          contentContainerStyle={$sectionListContentContainer}
-          stickySectionHeadersEnabled={false}
-          sections={Object.values(Demos)}
-          renderItem={({ item }) => item}
-          renderSectionFooter={() => <View style={$demoUseCasesSpacer} />}
-          ListHeaderComponent={
-            <View style={$heading}>
-              <Text preset="heading" tx="demoShowroomScreen.jumpStart" />
-            </View>
-          }
-          onScrollToIndexFailed={scrollToIndexFailed}
-          renderSectionHeader={({ section }) => {
-            return (
-              <View>
-                <Text preset="heading" style={$demoItemName}>
-                  {section.name}
-                </Text>
-                <Text style={$demoItemDescription}>{section.description}</Text>
-              </View>
-            )
-          }}
-        />
-      </Screen>
-    </DrawerLayout>
-  )
-}
 
 const $screenContainer: ViewStyle = {
   flex: 1,
