38c9a512ad5452a4c7da5977108e0ade6951a0cc
Kate Kim <kateinkim@gmail.com>
2022-12-15 08:49:26 -0700

    Update boilerplate/app/models/AuthenticationStore.ts
    Co-authored-by: Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>

--- boilerplate/app/models/AuthenticationStore.ts
+++ boilerplate/app/models/AuthenticationStore.ts
@@ -45,8 +45,13 @@ export const AuthenticationStoreModel = types
     },
   }))
   .preProcessSnapshot((snapshot) => {
-    // remove sensitive data from snapshot
+    // remove sensitive data from snapshot to avoid secrets
+    // being stored in AsyncStorage in plain text if backing up store
     const { authToken, authPassword, ...rest } = snapshot // eslint-disable-line @typescript-eslint/no-unused-vars
+    
+    // see the following for strategies to consider storing secrets on device
+    // https://reactnative.dev/docs/security#storing-sensitive-info
+    
     return rest
   })
 
