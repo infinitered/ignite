bbdf700a6766c521a6f2bef9e105f56f47424a8f
lodev09 <lodev09@gmail.com>
2023-01-20 07:17:29 +0800

    refactor: remove authPassword from store

--- boilerplate/app/models/AuthenticationStore.ts
+++ boilerplate/app/models/AuthenticationStore.ts
@@ -5,27 +5,17 @@ export const AuthenticationStoreModel = types
   .props({
     authToken: types.maybe(types.string),
     authEmail: "",
-    authPassword: "",
   })
   .views((store) => ({
     get isAuthenticated() {
       return !!store.authToken
     },
-    get validationErrors() {
-      return {
-        authEmail: (function () {
-          if (store.authEmail.length === 0) return "can't be blank"
-          if (store.authEmail.length < 6) return "must be at least 6 characters"
-          if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(store.authEmail))
-            return "must be a valid email address"
-          return ""
-        })(),
-        authPassword: (function () {
-          if (store.authPassword.length === 0) return "can't be blank"
-          if (store.authPassword.length < 6) return "must be at least 6 characters"
-          return ""
-        })(),
-      }
+    get validationError() {
+      if (store.authEmail.length === 0) return "can't be blank"
+      if (store.authEmail.length < 6) return "must be at least 6 characters"
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(store.authEmail))
+        return "must be a valid email address"
+      return ""
     },
   }))
   .actions((store) => ({
@@ -35,25 +25,11 @@ export const AuthenticationStoreModel = types
     setAuthEmail(value: string) {
       store.authEmail = value.replace(/ /g, "")
     },
-    setAuthPassword(value: string) {
-      store.authPassword = value.replace(/ /g, "")
-    },
     logout() {
       store.authToken = undefined
       store.authEmail = ""
-      store.authPassword = ""
     },
   }))
-  .preProcessSnapshot((snapshot) => {
-    // remove sensitive data from snapshot to avoid secrets
-    // being stored in AsyncStorage in plain text if backing up store
-    const { authToken, authPassword, ...rest } = snapshot // eslint-disable-line @typescript-eslint/no-unused-vars
-
-    // see the following for strategies to consider storing secrets on device
-    // https://reactnative.dev/docs/security#storing-sensitive-info
-
-    return rest
-  })
 
 export interface AuthenticationStore extends Instance<typeof AuthenticationStoreModel> {}
 export interface AuthenticationStoreSnapshot extends SnapshotOut<typeof AuthenticationStoreModel> {}
