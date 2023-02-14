978acf793153c66f89f6b9ff5ebeb6a70b3be9e3
Frank Calise <fcalise@gmail.com>
2023-02-13 23:10:52 -0500

    feat(ios): react native template updates

--- boilerplate/ios/HelloWorld/AppDelegate.h
+++ boilerplate/ios/HelloWorld/AppDelegate.h
@@ -1,9 +1,7 @@
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 #import <Expo/Expo.h>
 
-@interface AppDelegate : EXAppDelegateWrapper <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : EXAppDelegateWrapper
 
 @end
