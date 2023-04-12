8072e1c7ca47162d5194cf24bf51aec4cb2f5f13
Sergio <smaisidoro@gmail.com>
2023-04-12 22:14:51 +0300

    Sets locale dynamically if system uses a regional language

--- boilerplate/app/i18n/i18n.ts
+++ boilerplate/app/i18n/i18n.ts
@@ -8,14 +8,24 @@ import ar from "./ar"
 import ko from "./ko"
 
 i18n.fallbacks = true
-/**
- * we need always include "*-US" for some valid language codes because when you change the system language,
- * the language code is the suffixed with "-US". i.e. if a device is set to English ("en"),
- * if you change to another language and then return to English language code is now "en-US".
- */
 i18n.translations = { ar, en, "en-US": en, ko }
 
-i18n.locale = Localization.locale
+const fallbackLocale = "en"
+const systemLocale = Localization.locale
+
+if (i18n.translations.hasOwnProperty(systemLocale)) {
+  // if specific locales like en-FI or en-US is available, set it
+  i18n.locale = systemLocale
+} else {
+  // otherwise try to fallback to the general locale (dropping the -XX suffix)
+  // and trying
+  const generalLocale = systemLocale.split("-")[0]
+  if (i18n.translations.hasOwnProperty(generalLocale)) {
+    i18n.locale = generalLocale
+  } else {
+    i18n.locale = fallbackLocale
+  } 
+}
 
 // handle RTL languages
 export const isRTL = Localization.isRTL
