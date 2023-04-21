46ee84b7b9c12f35a1bb5a444e0a552c90213f96
Mehdi Idrizi <56638730+midrizi@users.noreply.github.com>
2023-04-21 18:35:48 +0200

    Fix getGeneralApiProblem return type (#2420 by @midrizi)
    [skip ci]

--- boilerplate/app/services/api/apiProblem.ts
+++ boilerplate/app/services/api/apiProblem.ts
@@ -43,7 +43,7 @@ export type GeneralApiProblem =
  *
  * @param response The api response.
  */
-export function getGeneralApiProblem(response: ApiResponse<any>): GeneralApiProblem | void {
+export function getGeneralApiProblem(response: ApiResponse<any>): GeneralApiProblem | null {
   switch (response.problem) {
     case "CONNECTION_ERROR":
       return { kind: "cannot-connect", temporary: true }
