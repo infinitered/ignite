a2c5ce909a6f7c34c82bf4aa13795d896a91525a
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-04-21 14:03:27 -0700

    Remove duplicated babel.config.js plugins, add comments

--- boilerplate/babel.config.js
+++ boilerplate/babel.config.js
@@ -1,5 +1,7 @@
+/** @type {import('@babel/core').TransformOptions['plugins']} */
 const plugins = [
   [
+    /** Enables baseUrl: "./" option in tsconfig.json to work @see https://github.com/entwicklerstube/babel-plugin-root-import */
     "babel-plugin-root-import",
     {
       paths: [
@@ -14,23 +16,17 @@ const plugins = [
       ],
     },
   ],
-  [
-    "@babel/plugin-proposal-decorators",
-    {
-      legacy: true,
-    },
-  ],
-  ["@babel/plugin-proposal-optional-catch-binding"],
+  /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web */
   "@babel/plugin-proposal-export-namespace-from",
-  "react-native-reanimated/plugin", // NOTE: this must be last in the plugins
+  /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
+  "react-native-reanimated/plugin",
 ]
 
-const babelConfig = {
+/** @type {import('@babel/core').TransformOptions} */
+module.exports = {
   presets: ["babel-preset-expo"],
   env: {
     production: {},
   },
   plugins,
 }
-
-module.exports = babelConfig
