414622a06680f8fe0c86c5c86dbb41109a97800d
Frank Calise <fcalise@gmail.com>
2023-04-21 11:08:02 -0400

    fix(boilerplate): vanilla Android template adjustment

--- boilerplate/android/app/src/release/java/com/helloworld/ReactNativeFlipper.java
+++ boilerplate/android/app/src/release/java/com/helloworld/ReactNativeFlipper.java
@@ -5,8 +5,10 @@
  * directory of this source tree.
  */
 package com.helloworld;
+
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
+
 /**
  * Class responsible of loading Flipper inside your React Native application. This is the release
  * flavor of it so it's empty as we don't want to load Flipper.
