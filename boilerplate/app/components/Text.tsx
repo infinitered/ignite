2e96d786fed8671b2cdbb5e85cd57c3142b61c90
Frank Calise <fcalise@gmail.com>
2023-05-23 07:02:09 -0400

    fix(boilerplate): update Text sizeStyles to use satisfies (#2437 by @frankcalise)
    [skip ci]
    
    * chore: upgrade TS to match VS Code version
    
    * fix(boilerplate): update Text size styles with satisfies
    
    * vendor: updated some dev deps for satisfies support
    
    * chore: match boilerplate dev deps for TS checks
    
    * chore: lint fix
    
    * updated yarn lock

--- boilerplate/app/components/Text.tsx
+++ boilerplate/app/components/Text.tsx
@@ -73,13 +73,13 @@ export function Text(props: TextProps) {
 }
 
 const $sizeStyles = {
-  xxl: { fontSize: 36, lineHeight: 44 } as TextStyle,
-  xl: { fontSize: 24, lineHeight: 34 } as TextStyle,
-  lg: { fontSize: 20, lineHeight: 32 } as TextStyle,
-  md: { fontSize: 18, lineHeight: 26 } as TextStyle,
-  sm: { fontSize: 16, lineHeight: 24 } as TextStyle,
-  xs: { fontSize: 14, lineHeight: 21 } as TextStyle,
-  xxs: { fontSize: 12, lineHeight: 18 } as TextStyle,
+  xxl: { fontSize: 36, lineHeight: 44 } satisfies TextStyle,
+  xl: { fontSize: 24, lineHeight: 34 } satisfies TextStyle,
+  lg: { fontSize: 20, lineHeight: 32 } satisfies TextStyle,
+  md: { fontSize: 18, lineHeight: 26 } satisfies TextStyle,
+  sm: { fontSize: 16, lineHeight: 24 } satisfies TextStyle,
+  xs: { fontSize: 14, lineHeight: 21 } satisfies TextStyle,
+  xxs: { fontSize: 12, lineHeight: 18 } satisfies TextStyle,
 }
 
 const $fontWeightStyles = Object.entries(typography.primary).reduce((acc, [weight, fontFamily]) => {
