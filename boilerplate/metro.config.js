d68f34c7ee77a9d8b0cc2358078d0cf8694b3297
Frank Calise <fcalise@gmail.com>
2023-05-24 13:13:36 -0400

    chore: RN 0.72.0-rc.3 and Expo 49 alpha.3

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -1,6 +1,7 @@
-const { getDefaultConfig } = require("metro-config")
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { getDefaultConfig: getDefaultExpoConfig } = require("@expo/metro-config")
 
+const config = {}
 let metroConfig
 let isExpo = false
 try {
@@ -21,6 +22,7 @@ if (isExpo) {
    */
   metroConfig = getDefaultExpoConfig(__dirname)
 } else {
+  metroConfig = mergeConfig(getDefaultConfig(__dirname), config);
   /**
    * Vanilla metro config - we're using a custom metro config because we want to support symlinks
    * out of the box. This allows you to use pnpm and/or play better in a monorepo.
@@ -30,25 +32,25 @@ if (isExpo) {
    *
    * However, it doesn't hurt to have it either.
    */
-  const { makeMetroConfig } = require("@rnx-kit/metro-config")
-  const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
+  // const { makeMetroConfig } = require("@rnx-kit/metro-config")
+  // const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
 
-  metroConfig = (async () => {
-    const defaultConfig = await getDefaultConfig()
-    return makeMetroConfig({
-      projectRoot: __dirname,
-      // watchFolders: [`${__dirname}/../..`], // for monorepos
-      resolver: {
-        /**
-         * This custom resolver is for if you're using symlinks.
-         *
-         * You can disable it if you're not using pnpm or a monorepo or symlinks.
-         */
-        resolveRequest: MetroSymlinksResolver(),
-        assetExts: [...defaultConfig.resolver.assetExts, "bin"],
-      },
-    })
-  })()
+  // metroConfig = (async () => {
+  //   const defaultConfig = await getDefaultConfig()
+  //   return makeMetroConfig({
+  //     projectRoot: __dirname,
+  //     // watchFolders: [`${__dirname}/../..`], // for monorepos
+  //     resolver: {
+  //       /**
+  //        * This custom resolver is for if you're using symlinks.
+  //        *
+  //        * You can disable it if you're not using pnpm or a monorepo or symlinks.
+  //        */
+  //       resolveRequest: MetroSymlinksResolver(),
+  //       assetExts: [...defaultConfig.resolver.assetExts, "bin"],
+  //     },
+  //   })
+  // })()
 }
 
 module.exports = metroConfig
