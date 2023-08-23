9a3709382f71cec1cb2c8e8794857a2570f23bdc
Frank Calise <fcalise@gmail.com>
2023-08-23 16:12:06 -0400

    fix(boilerplate): better new arch support

---
+++ boilerplate/metro.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+}
