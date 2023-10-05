2630214ed7e7600259b67150b3ea58b0e06de579
Mark Rickert <mjar81@gmail.com>
2023-10-05 16:51:05 -0600

    fix: Get FlashList working in new architecture using interop layer
    iOS seems to have problems with flashlist using interop, but android seems to work just fine.

--- boilerplate/react-native.config.js
+++ boilerplate/react-native.config.js
@@ -5,4 +5,18 @@ module.exports = {
     // packages, you can add them to the referenced path below and uncomment this line.
     // "./assets/fonts/"
   ],
+
+  // This prevents FlashList from rendering a large red box.
+  // See: https://github.com/reactwg/react-native-new-architecture/discussions/135
+  // Remove when FlashList fully supports the new architecture.
+  // https://github.com/Shopify/flash-list/pull/550
+  //
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: ["CellContainer", "AutoLayoutView"],
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: ["CellContainer", "AutoLayoutView"],
+    },
+  },
 }
