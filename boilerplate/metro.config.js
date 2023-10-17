c484e3dd91a25fefe4c9909032d021dd21e6147b
Frank Calise <fcalise@gmail.com>
2023-10-16 22:18:59 -0400

    fix(boilerplate): inherit default metro from expo (#2526 by @frankcalise)

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
   },
-}
+});
+
+module.exports = config;
