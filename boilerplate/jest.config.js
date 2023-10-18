43f2714f0ba4c51e192c35b1037a3924f093d4fb
Frank Calise <fcalise@gmail.com>
2023-10-17 23:01:50 -0400

    fix(jest): map ts paths

--- boilerplate/jest.config.js
+++ boilerplate/jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.maestro/", "@react-native"],
   testEnvironment: "jsdom",
   setupFiles: ["<rootDir>/test/setup.ts"],
+  moduleNameMapper: { "^app/(.*)$": "<rootDir>/app/$1", "^assets/(.*)$": "<rootDir>/assets/$1" },
 }
