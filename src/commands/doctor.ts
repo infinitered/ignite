82203a4f523a9a42deff683c287ef014bd0796dc
jvm-cognition <imaginedoingwork@gmail.com>
2023-10-24 01:09:46 +0200

    refactor(doctor command): added indentation to first column to align with spinners for new command

--- src/commands/doctor.ts
+++ src/commands/doctor.ts
@@ -6,6 +6,7 @@
 import { GluegunToolbox } from "gluegun"
 import * as os from "os"
 import { packager } from "../tools/packager"
+import { INDENT } from "../tools/pretty"
 
 const isWindows = process.platform === "win32"
 const isMac = process.platform === "darwin"
@@ -23,7 +24,7 @@ module.exports = {
     } = toolbox
 
     // display helpers
-    const column1 = (label, length = 16) => padEnd(label || "", length)
+    const column1 = (label, length = 16) => padEnd(`${INDENT} ${label}` || "", length)
     const column2 = (label) => colors.yellow(padEnd(label || "-", 10))
     const column3 = (label) => colors.muted(label)
 
@@ -36,7 +37,7 @@ module.exports = {
     const cores = `${cpus.length} cores`
     const directory = `${process.cwd()}`
 
-    info(colors.cyan("System"))
+    info(colors.cyan(`${INDENT} System`))
     table([
       [column1("platform"), column2(platform), column3("")],
       [column1("arch"), column2(arch), column3("")],
@@ -96,7 +97,7 @@ module.exports = {
     const expoInfo = [column1("expo"), column2(expoVersion), column3(expoWorkflow)]
 
     info("")
-    info(colors.cyan(`JavaScript${haveGlobalPackages ? " (and globally-installed packages)" : ""}`))
+    info(colors.cyan(`${INDENT} JavaScript${haveGlobalPackages ? " (and globally-installed packages)" : ""}`))
     table([
       nodeInfo,
       npmInfo,
@@ -114,7 +115,7 @@ module.exports = {
     const igniteVersion = meta.version()
 
     info("")
-    info(colors.cyan("Ignite"))
+    info(colors.cyan(`${INDENT} Ignite`))
     const igniteTable = []
     igniteTable.push([column1("ignite-cli"), column2(igniteVersion), column3(ignitePath)])
     igniteTable.push([
@@ -138,7 +139,7 @@ module.exports = {
     }
 
     info("")
-    info(colors.cyan("Android"))
+    info(colors.cyan(`${INDENT} Android`))
     table([
       [column1("java"), column2(javaVersion), column3(javaPath)],
       [column1("android home"), column2("-"), column3(androidPath)],
@@ -151,7 +152,7 @@ module.exports = {
         xcodePath && (await run("xcodebuild -version", { trim: true })).split(/\s/)[1]
 
       info("")
-      info(colors.cyan("iOS"))
+      info(colors.cyan(`${INDENT} iOS`))
       table([[column1("xcode"), column2(xcodeVersion)]])
 
       const cocoaPodsPath = which("pod") || ""
@@ -171,7 +172,7 @@ module.exports = {
 
     // -=-=-=- tools -=-=-=-
     info("")
-    info(colors.cyan("Tools"))
+    info(colors.cyan(`${INDENT} Tools`))
     const gitPath = which("git")
     const gitVersion = gitPath && (await run("git --version", { trim: true }))
     const gitInfo = [column1("git"), column2(gitVersion), column3(gitPath || "not installed")]
