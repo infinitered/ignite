52117013543f604680db5598eba363cb9cae51fb
Frank Calise <fcalise@gmail.com>
2023-11-14 21:48:14 -0500

    chore: tsc fixes

--- boilerplate/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
+++ boilerplate/app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
@@ -173,7 +173,7 @@ export const DemoShowroomScreen: FC<DemoTabScreenProps<"DemoShowroom">> =
         )}
       >
         <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
-          <DrawerIconButton onPress={toggleDrawer} {...{ open }} />
+          <DrawerIconButton onPress={toggleDrawer} />
 
           <SectionList
             ref={listRef}
