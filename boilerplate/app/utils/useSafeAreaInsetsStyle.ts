9efddf07ce651d2a4712f066c10f54d9ef761152
Rhys <47733004+RhyG@users.noreply.github.com>
2024-01-19 02:31:06 +1000

    chore(docs): fixed doc link inside `useSafeAreaInsetsStyle` (#2610)

--- boilerplate/app/utils/useSafeAreaInsetsStyle.ts
+++ boilerplate/app/utils/useSafeAreaInsetsStyle.ts
@@ -20,7 +20,7 @@ const edgeInsetMap: Record<string, Edge> = {
 /**
  * A hook that can be used to create a safe-area-aware style object that can be passed directly to a View.
  *
- * - [Documentation and Examples](https://github.com/infinitered/ignite/blob/master/docs/Utils-useSafeAreaInsetsStyle.md)
+ * - [Documentation and Examples](https://github.com/infinitered/ignite/blob/master/docs/boilerplate/utility/useSafeAreaInsetsStyle.md)
  */
 export function useSafeAreaInsetsStyle(
   safeAreaEdges: ExtendedEdge[] = [],
