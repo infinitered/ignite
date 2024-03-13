1c1b907e4a5c811bfc6d49137a368f541c3509d3
Frank Calise <fcalise@gmail.com>
2024-03-13 10:03:01 -0400

    fix(babel-config): simplify plugins with SDK 50

--- boilerplate/babel.config.js
+++ boilerplate/babel.config.js
@@ -1,11 +1,3 @@
-/** @type {import('@babel/core').TransformOptions['plugins']} */
-const plugins = [
-  /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web */
-  "@babel/plugin-proposal-export-namespace-from",
-  /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
-  "react-native-reanimated/plugin",
-]
-
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = function(api) {
   api.cache(true);
@@ -14,6 +6,5 @@ module.exports = function(api) {
     env: {
       production: {},
     },
-    plugins,
   };
 };
