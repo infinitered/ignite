d7a32209ec77c121ca86e0ac2349ea7ba9ae2471
Frank Calise <fcalise@gmail.com>
2024-04-04 15:03:40 -0400

    docs(api): fix backend integration link (#2662 by @frankcalise)
    [skip ci]

--- boilerplate/app/services/api/api.ts
+++ boilerplate/app/services/api/api.ts
@@ -2,7 +2,7 @@
  * This Api class lets you define an API endpoint and methods to request
  * data and process it.
  *
- * See the [Backend API Integration](https://docs.infinite.red/ignite-cli/boilerplate/app/services/Services.md)
+ * See the [Backend API Integration](https://docs.infinite.red/ignite-cli/boilerplate/app/services/#backend-api-integration)
  * documentation for more details.
  */
 import {
