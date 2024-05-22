cf71268ec731c4de47ee95154b5138b65eb6f18c
Jamon Holmgren <code@jamon.dev>
2024-05-22 15:57:49 -0700

    fix(metro): Add support for cjs extension in metro.config.js

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -6,9 +6,19 @@ const config = getDefaultConfig(__dirname);
 
 config.transformer.getTransformOptions = async () => ({
   transform: {
+    // TODO: Document why we added this
     experimentalImportSupport: false,
+
+    // Inline requires are very useful for deferring loading of large
+    // dependencies / components. However, they come with some gotchas.
+    // Read more here: https://reactnative.dev/docs/optimizing-javascript-loading
+    // And here: https://github.com/expo/expo/issues/27279#issuecomment-1971610698
     inlineRequires: true,
   },
 });
+
+// This helps support certain popular third-party libraries
+// such as Firebase that use the extension cjs.
+config.resolver.sourceExts.push("cjs")
 
 module.exports = config;
