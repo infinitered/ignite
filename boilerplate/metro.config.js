7544faced40e26767ce7454eae4538ae9ccb85ee
Jamon Holmgren <code@jamon.dev>
2024-05-28 17:52:43 -0700

    fix(metro): Add support for cjs extension in metro.config.js (#2689 by @jamonholmgren)
    * Add support for cjs extension in metro.config.js
    * Add comments about inlineRequires and Reactotron
    * Remove `experimentalInlineRequires` from metro config
    
    [skip ci]

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -6,9 +6,17 @@ const config = getDefaultConfig(__dirname);
 
 config.transformer.getTransformOptions = async () => ({
   transform: {
-    experimentalImportSupport: false,
+    // Inline requires are very useful for deferring loading of large dependencies/components.
+    // For example, we use it in app.tsx to conditionally load Reactotron.
+    // However, this comes with some gotchas.
+    // Read more here: https://reactnative.dev/docs/optimizing-javascript-loading
+    // And here: https://github.com/expo/expo/issues/27279#issuecomment-1971610698
     inlineRequires: true,
   },
 });
+
+// This helps support certain popular third-party libraries
+// such as Firebase that use the extension cjs.
+config.resolver.sourceExts.push("cjs")
 
 module.exports = config;
