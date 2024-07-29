bc2bf8c1ff793e0df197226884427a7fbb2ee957
Felipe Peña <felipe@penya.cl>
2024-07-29 13:37:37 -0700

    Add new file to define common styles

--- boilerplate/app/theme/index.ts
+++ boilerplate/app/theme/index.ts
@@ -1,4 +1,5 @@
 export * from "./colors"
 export * from "./spacing"
+export * from "./styles"
 export * from "./typography"
 export * from "./timing"
---
+++ boilerplate/app/theme/styles.ts
@@ -0,0 +1,3 @@
+/* Use this file to define styles that are used in multiple places in your app. */
+export const $styles = {
+}
