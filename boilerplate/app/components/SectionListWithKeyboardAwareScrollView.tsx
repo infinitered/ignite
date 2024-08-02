5be7fa9ed9161e845efedbb25b3abc28b49aa035
Felipe Peña <felipe@penya.cl>
2024-08-02 11:33:21 -0700

    Add documentation

--- boilerplate/app/components/SectionListWithKeyboardAwareScrollView.tsx
+++ boilerplate/app/components/SectionListWithKeyboardAwareScrollView.tsx
@@ -11,19 +11,46 @@ type SectionType<ItemType> = {
 }
 
 type SectionListWithKeyboardAwareScrollViewProps<ItemType> = SectionListProps<ItemType> & {
-  renderScrollComponent?: () => React.ReactNode,
+  /* Optional function to pass a custom scroll component */
+  renderScrollComponent?: () => React.ReactNode
+  /* Optional bottom offset for the keyboard avoid behavior */
   bottomOffset?: number
+  /* The sections to be rendered in the list */
   sections: SectionType<ItemType>[]
+  /* Function to render the header for each section */
   renderSectionHeader: ({ section }: { section: SectionType<ItemType> }) => React.ReactNode
 }
 
+/**
+ * SectionListWithKeyboardAwareScrollView
+ *
+ * A component that combines a SectionList with a KeyboardAwareScrollView to handle
+ * keyboard interactions smoothly. This component is useful when you have a list of
+ * sections and need to ensure that the keyboard does not cover the input fields.
+ *
+ * Props:
+ * @param {Object} props - The properties object.
+ * @param {Array} props.sections - The sections to be rendered in the list.
+ * @param {Function} props.renderSectionHeader - Function to render the header for each section.
+ * @param {Object} props.sectionListProps - Additional props for SectionList.
+ *
+ * @returns {JSX.Element} The rendered component.
+ */
 export const SectionListWithKeyboardAwareScrollView = forwardRef(
   <ItemType,>(
-    { renderScrollComponent, bottomOffset = DEFAULT_BOTTOM_OFFSET, contentContainerStyle, ...props }: SectionListWithKeyboardAwareScrollViewProps<ItemType>,
+    {
+      renderScrollComponent,
+      bottomOffset = DEFAULT_BOTTOM_OFFSET,
+      contentContainerStyle,
+      ...props
+    }: SectionListWithKeyboardAwareScrollViewProps<ItemType>,
     ref: React.Ref<SectionList<ItemType>>,
   ) => {
     const defaultRenderScrollComponent = () => (
-      <KeyboardAwareScrollView contentContainerStyle={contentContainerStyle} bottomOffset={bottomOffset} />
+      <KeyboardAwareScrollView
+        contentContainerStyle={contentContainerStyle}
+        bottomOffset={bottomOffset}
+      />
     )
 
     return (
