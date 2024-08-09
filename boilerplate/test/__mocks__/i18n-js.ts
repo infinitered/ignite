50c410545114a264f7484ac983d798bb0a0d613a
Frank Calise <fcalise@gmail.com>
2024-08-09 16:44:53 -0400

    fix(boilerplate): i18n jest mock

--- boilerplate/test/__mocks__/i18n-js.ts
+++
@@ -1,9 +0,0 @@
-export const I18n = jest.fn().mockImplementation(() => {
-  return {
-    locale: "en",
-    t: (key: string, params: Record<string, string>) => {
-      return `${key} ${JSON.stringify(params)}`
-    },
-    numberToCurrency: jest.fn(),
-  }
-})
