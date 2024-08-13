c7edf450a06c440039a8abe8b900e55d19fd4eee
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-08-13 05:23:45 -0700

    fix: support component testing (#2740 by @lindboe)
    There were some issues with out-of-date config when setting up component
    testing for the first time.
    
    This PR removes old configuration that's no longer necessary, and adds
    tests to ensure component testing continues to work.

--- boilerplate/jest.config.js
+++ boilerplate/jest.config.js
@@ -1,34 +1,5 @@
-const { defaults: tsjPreset } = require("ts-jest/presets")
-
-const thirdPartyIgnorePatterns = [
-  "((jest-)?react-native|@react-native(-community)?)",
-  "expo(nent)?",
-  "@expo(nent)?/.*",
-  "@expo-google-fonts/.*",
-  "react-navigation",
-  "@react-navigation/.*",
-  "@unimodules/.*",
-  "unimodules",
-  "sentry-expo",
-  "native-base",
-  "react-native-svg",
-  "react-clone-referenced-element",
-  "react-native-code-push",
-]
-
 /** @type {import('@jest/types').Config.ProjectConfig} */
 module.exports = {
-  ...tsjPreset,
   preset: "jest-expo",
-  transformIgnorePatterns: [
-    `<rootDir>/node_modules/(?!${thirdPartyIgnorePatterns.join("|")})`,
-    "jest-runner",
-  ],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.maestro/", "@react-native"],
   setupFiles: ["<rootDir>/test/setup.ts"],
-  transform:{
-    '^.+\\.test.tsx?$': ['ts-jest', {
-      tsconfig: '<rootDir>/test/test-tsconfig.json'
-    }]
-  }
 }
