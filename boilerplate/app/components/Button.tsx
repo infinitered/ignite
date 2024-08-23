7349331815c75f95103752426b7982b55d20df10
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-08-22 23:47:42 -0400

    docs: Fixed documentation link in Button (#2755 by @softwarebyze)

--- boilerplate/app/components/Button.tsx
+++ boilerplate/app/components/Button.tsx
@@ -84,7 +84,7 @@ export interface ButtonProps extends PressableProps {
 /**
  * A component that allows users to take actions and make choices.
  * Wraps the Text component with a Pressable component.
- * @see [Documentation and Examples]{@link https://docs.infinite.red/ignite-cli/boilerplate/components/Button/}
+ * @see [Documentation and Examples]{@link https://docs.infinite.red/ignite-cli/boilerplate/app/components/Button/}
  * @param {ButtonProps} props - The props for the `Button` component.
  * @returns {JSX.Element} The rendered `Button` component.
  * @example
