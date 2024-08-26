fb505d590beeeb1e0a11a10015ffc00a0333e2b6
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-08-26 15:50:24 -0700

    Configure expo default prettier

--- boilerplate/.eslintrc.js
+++ boilerplate/.eslintrc.js
@@ -1,4 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
