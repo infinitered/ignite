61747804f330f0054e96ba1fcc52c065f7abd663
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-08-26 16:00:07 -0700

    Update config files to match lint rules
    Was ignored by linting before.
    
    Ignoring files may be useful if they are part of a template, and we
    never plan to customize the file; but if we do, linters may be useful
    for catching bugs.

--- boilerplate/.eslintrc.js
+++ boilerplate/.eslintrc.js
@@ -1,8 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
-};
+}
