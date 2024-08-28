dff3d249160450da84c1bde9d73012923559a35c
Frank Calise <fcalise@gmail.com>
2024-08-28 15:21:32 -0400

    feat(cli): generator options --dir and --case  (#2726 by @frankcalise)
    * fix(boilerplate): generator destinationDir by default
    
    * fix(boilerplate): remove destinationDir from component default generator
    
    * fix(cli): update help with destinationDir example
    
    * feat(cli): add originalName, destinationDir and exact options for generation
    
    * feat(cli): implemented --destinationDir and --exact
    
    * docs: updated generator docs for new cli options
    
    * test(generate): match snapshot for expo router test
    
    * docs(generators): punctutation
    
    Co-authored-by: Lizzi Lindboe <lindboe@users.noreply.github.com>
    
    * fix(cli): destinationDir -> dir
    
    * fix(cli): exact -> case
    
    ---------
    
    Co-authored-by: Lizzi Lindboe <lindboe@users.noreply.github.com>

--- test/_test-helpers.ts
+++ test/_test-helpers.ts
@@ -31,3 +31,91 @@ export async function runError(cmd: string): Promise<string | any> {
   }
   return `No error thrown? Output: ${resultANSI}`
 }
+
+function generateDefaultTemplatePath(pathname: string): string {
+  return filesystem.path(pathname, "ignite", "templates", "screen", "NAMEScreen.tsx.ejs")
+}
+
+function generateExpoRouterTemplatePath(pathname: string): string {
+  return filesystem.path(pathname, "ignite", "templates", "screen", "NAME.tsx.ejs")
+}
+
+export function copyDefaultScreenGenerator(tempBoilerplatePath: string): void {
+  const REACT_NAVIGATION_SCREEN_TPL = `---
+destinationDir: app/screens
+patches:
+- path: "app/screens/index.ts"
+  append: "export * from "./<%= props.pascalCaseName %>Screen"\n"
+  skip: <%= props.skipIndexFile %>
+- path: "app/navigators/AppNavigator.tsx"
+  replace: "// IGNITE_GENERATOR_ANCHOR_APP_STACK_PARAM_LIST"
+  insert: "<%= props.pascalCaseName %>: undefined\n\t// IGNITE_GENERATOR_ANCHOR_APP_STACK_PARAM_LIST"
+- path: "app/navigators/AppNavigator.tsx"
+  replace: "{/* IGNITE_GENERATOR_ANCHOR_APP_STACK_SCREENS */}"
+  insert: "<Stack.Screen name="<%= props.pascalCaseName %>" component={Screens.<%= props.pascalCaseName %>Screen} />\n\t\t\t{/* IGNITE_GENERATOR_ANCHOR_APP_STACK_SCREENS */}"
+  skip: <%= props.skipIndexFile %>
+---
+import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle } from "react-native"
+import { AppStackScreenProps } from "app/navigators"
+import { Screen, Text } from "app/components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "app/models"
+
+interface <%= props.pascalCaseName %>ScreenProps extends AppStackScreenProps<"<%= props.pascalCaseName %>"> {}
+
+export const <%= props.pascalCaseName %>Screen: FC<<%= props.pascalCaseName %>ScreenProps> = observer(function <%= props.pascalCaseName %>Screen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={$root} preset="scroll">
+      <Text text="<%= props.camelCaseName %>" />
+    </Screen>
+  )
+})
+
+const $root: ViewStyle = {
+  flex: 1,
+}
+`
+
+  const destination = generateDefaultTemplatePath(tempBoilerplatePath)
+  filesystem.write(destination, REACT_NAVIGATION_SCREEN_TPL)
+}
+
+export function copyExpoRouterScreenGenerator(tempBoilerplatePath: string): void {
+  const EXPO_ROUTER_SCREEN_TPL = `import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle } from "react-native"
+import { Screen, Text } from "app/components"
+
+export default observer(function <%= props.pascalCaseName %>Screen() {
+  return (
+    <Screen style={$root} preset="scroll">
+      <Text text="<%= props.camelCaseName %>" />
+    </Screen>
+  )
+})
+
+const $root: ViewStyle = {
+  flex: 1,
+}
+`
+
+  const destination = generateExpoRouterTemplatePath(tempBoilerplatePath)
+  filesystem.write(destination, EXPO_ROUTER_SCREEN_TPL)
+}
+
+export function removeDefaultScreenGenerator(tempBoilerplatePath: string): void {
+  const destination = generateDefaultTemplatePath(tempBoilerplatePath)
+  filesystem.remove(destination)
+}
+
+export function removeExpoRouterScreenGenerator(tempBoilerplatePath: string): void {
+  const destination = generateExpoRouterTemplatePath(tempBoilerplatePath)
+  filesystem.remove(destination)
+}
