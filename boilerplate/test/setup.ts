01e578b0f8455f738c6d5677ff21cab1844c9b06
Felipe Peña <felipe@penya.cl>
2024-09-12 10:18:57 -0700

    Replace right mock

--- boilerplate/test/setup.ts
+++ boilerplate/test/setup.ts
@@ -23,12 +23,13 @@ jest.doMock("react-native", () => {
   )
 })
 
-jest.mock("i18next", () => ({
-  currentLocale: () => "en",
-  t: (key: string, params: Record<string, string>) => {
-    return `${key} ${JSON.stringify(params)}`
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  withTranslation: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: () => "" };
+    return Component;
   },
-}))
+}));
 
 jest.mock("expo-localization", () => ({
   ...jest.requireActual("expo-localization"),
