bc6caf9e4b167a21ae97d0b68aa4dc3f52d5ff7b
Frank Calise <fcalise@gmail.com>
2024-09-24 19:32:11 -0400

    fix(new): new arch compat libs

--- src/tools/dependencies.ts
+++ src/tools/dependencies.ts
@@ -24,3 +24,25 @@ export function removePackageJSONDependencies(
   const updatedPackageJson = findAndRemoveDependencies(packageJsonRaw, dependenciesToRemove)
   filesystem.write(packageJsonPath, updatedPackageJson)
 }
+
+export function findAndUpdateDependencyVersions(
+  packageJsonRaw: string,
+  dependencies: Record<string, string>,
+): string {
+  let updatedPackageJson = packageJsonRaw
+
+  Object.keys(dependencies).forEach((depName) => {
+    const desiredVersion = dependencies[depName]
+    const regex = new RegExp(`"${depName}"\\s*:\\s*"[^"]+"`, "g")
+    updatedPackageJson = updatedPackageJson.replace(regex, `"${depName}": "${desiredVersion}"`)
+  })
+
+  // Make sure `expo-dev-client` is removed
+  updatedPackageJson = updatedPackageJson.replace(/"expo-dev-client"\s*:\s*"[^"]+",?/g, "")
+
+  return updatedPackageJson
+}
+
+export const newArchCompatExpectedVersions = {
+  "react-native-mmkv": "3.0.1",
+}
