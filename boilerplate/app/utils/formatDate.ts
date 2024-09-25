42f0bd1c1c11c4cb298041a394606c07aa34abe0
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-09-24 17:01:36 -0700

    Upgrade date-fns

--- boilerplate/app/utils/formatDate.ts
+++ boilerplate/app/utils/formatDate.ts
@@ -1,20 +1,14 @@
-// Note the syntax of these imports from the date-fns library.
-// If you import with the syntax: import { format } from "date-fns" the ENTIRE library
-// will be included in your production bundle (even if you only use one function).
-// This is because react-native does not support tree-shaking.
-import type { Locale } from "date-fns"
-import format from "date-fns/format"
-import parseISO from "date-fns/parseISO"
-import ar from "date-fns/locale/ar-SA"
-import ko from "date-fns/locale/ko"
-import en from "date-fns/locale/en-US"
+import { format, parseISO, type Locale } from "date-fns"
+import { arSA } from "date-fns/locale/ar-SA"
+import { ko } from "date-fns/locale/ko"
+import { enUS } from "date-fns/locale/en-US"
 import { i18n } from "@/i18n"
 
 type Options = Parameters<typeof format>[2]
 
 const getLocale = (): Locale => {
   const locale = i18n.language.split("-")[0]
-  return locale === "ar" ? ar : locale === "ko" ? ko : en
+  return locale === "ar" ? arSA : locale === "ko" ? ko : enUS
 }
 
 export const formatDate = (date: string, dateFormat?: string, options?: Options) => {
