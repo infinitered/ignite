fe82fdba91bba64ab6dfc6ffaf32df53af50bc5a
Frank Calise <fcalise@gmail.com>
2025-01-10 08:55:53 -0500

    fix(boilerplate): main entry point for monorepo (#2881 by @frankcalise)

--- boilerplate/App.tsx
+++ boilerplate/App.tsx
@@ -1,4 +1,5 @@
 import "@expo/metro-runtime"
+import { registerRootComponent } from "expo"
 import * as SplashScreen from "expo-splash-screen"
 import App from "@/app"
 
@@ -8,4 +9,7 @@ function IgniteApp() {
   return <App hideSplashScreen={SplashScreen.hideAsync} />
 }
 
-export default IgniteApp
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(IgniteApp)
