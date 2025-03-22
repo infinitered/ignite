c1619f78f375f5b4302197daeced8857cf8c0828
Frank Calise <fcalise@gmail.com>
2025-03-22 15:13:55 -0400

    chore(cli): more error info

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1079,6 +1079,7 @@ module.exports = {
       p2(red(`\nThe following error occurred:`))
       p2()
       p2(red(e.toString()))
+      p2(red(e.stderr))
 
       p2()
       p2("Consider opening an issue with the following information at:")
