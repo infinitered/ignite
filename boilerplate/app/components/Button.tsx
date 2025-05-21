310abe1f8eef30239b2db15c4da5e7f9a566c44e
Danny Ricciotti <dan.ricciotti@gmail.com>
2025-05-21 13:15:22 -0400

    Fix: Update  to use TextStyle instead of ViewStyle (#2952) [skip ci]

--- boilerplate/app/components/Button.tsx
+++ boilerplate/app/components/Button.tsx
@@ -239,7 +239,7 @@ const $pressedViewPresets: Record<Presets, ThemedStyle<ViewStyle>> = {
   reversed: ({ colors }) => ({ backgroundColor: colors.palette.neutral700 }),
 }
 
-const $pressedTextPresets: Record<Presets, ThemedStyle<ViewStyle>> = {
+const $pressedTextPresets: Record<Presets, ThemedStyle<TextStyle>> = {
   default: () => ({ opacity: 0.9 }),
   filled: () => ({ opacity: 0.9 }),
   reversed: () => ({ opacity: 0.9 }),
