87d8f2b02e1680d70ae9de18c81b56925baa3300
Frank Calise <fcalise@gmail.com>
2025-05-22 12:08:44 -0400

    fix(cli): support for yarn4 (#2913 by @frankcalise @markrickert)
    * also updates the repo to utilize yarn4 
    
    * chore(pkg-mgr): update to yarn4
    
    * fix(new): support yarn4 via blank yarn.lock
    
    * fix(new): more yarn4 support
    
    * fix(cli): yarn4 support
    
    * fix(cli): doctor cmd for yarn4 projects
    
    removal of global expo info
    
    * chore(cli): more error info
    
    * debugging output
    
    * fix(boilerplate): expected @types/node dev dep for ts-node in yarn2+
    
    * fix(cli): --no-immutable for yarn2+
    
    * ci(cli): only do --no-immutable to satisfy CI
    
    * ci: yarn2+ install flags
    
    * test: restore cleanup
    
    * Update .gitignore
    
    Co-authored-by: Tyler Scott Williams <tyler@coolsoftware.dev>
    
    * Update boilerplate/.gitignore
    
    Co-authored-by: Tyler Scott Williams <tyler@coolsoftware.dev>
    
    * chore(yarn): upgrade to latest stable yarn: yarn set version 4.9.1
    
    ---------
    
    Co-authored-by: Tyler Scott Williams <tyler@coolsoftware.dev>
    Co-authored-by: Mark Rickert <mjar81@gmail.com>

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -687,6 +687,7 @@ module.exports = {
       // pnpm/yarn/npm/bun install it
 
       // fix .npmrc if using pnpm
+      let installFlags = ""
       if (packagerName === "pnpm") {
         // append `node-linker=hoisted` to .npmrc
         const npmrcPath = path(targetPath, ".npmrc")
@@ -698,11 +699,18 @@ module.exports = {
 
         // if yarn version > 1 fix .yarnrc.yml
         if (yarnMajorVersion > 1) {
+          if (process.env.CI === "true") {
+            installFlags = " --no-immutable"
+          }
           log(`yarn v${yarnMajorVersion} found... fixing .yarnrc.yml...`)
           // append `nodeLinker: node-modules` to .yarnrc.yml
           const yarnrcPath = path(targetPath, ".yarnrc.yml")
           const yarnrcContents = read(yarnrcPath)
           write(yarnrcPath, `${yarnrcContents ?? ""}${EOL}nodeLinker: node-modules${EOL}`)
+          // also create a blank yarn.lock file to avoid workspaces issue
+          write(path(targetPath, "yarn.lock"), "")
+          // update the `packagerManager` field in `package.json
+          await system.run(`yarn set version ${yarnVersion}`, { onProgress: log })
         }
       }
 
@@ -757,7 +765,7 @@ module.exports = {
 
         // do base install
         const installCmd = packager.installCmd({ packagerName })
-        await system.run(installCmd, { onProgress: log })
+        await system.run(`${installCmd}${installFlags}`, { onProgress: log })
         // now that expo is installed, we can run their install --fix for best Expo SDK compatibility
         // for right now, we don't do this in CI because it returns a non-zero exit code
         // see https://docs.expo.dev/more/expo-cli/#version-validation
@@ -789,7 +797,7 @@ module.exports = {
 
       // #region Configure app.json
       // Enable New Architecture if requested (must happen before prebuild)
-      startSpinner("Configuring app.json")
+      startSpinner(" Configuring app.json")
       try {
         const appJsonRaw = read("app.json")
         const appJson = JSON.parse(appJsonRaw)
@@ -811,7 +819,7 @@ module.exports = {
         log(e)
         p(yellow("Unable to configure app.json."))
       }
-      stopSpinner("Configuring app.json", "⚙️")
+      stopSpinner(" Configuring app.json", "⚙️")
       // #endregion
 
       // #region Run Prebuild
@@ -819,7 +827,7 @@ module.exports = {
       if (installDeps === true) {
         // Check if we need to run prebuild to generate native dirs based on workflow
         // Prebuild also handles the packager install
-        const prebuildMessage = `Generating native template via Expo Prebuild`
+        const prebuildMessage = ` Generating native template via Expo Prebuild`
         startSpinner(prebuildMessage)
         await packager.run("prebuild:clean", { ...packagerOptions, onProgress: log })
         stopSpinner(prebuildMessage, "🛠️")
@@ -828,11 +836,7 @@ module.exports = {
 
       // #region Remove Demo code
       const removeDemoPart = removeDemo === true ? "code" : "markup"
-      startSpinner(`Removing fancy demo ${removeDemoPart}`)
-
-      p(yellow(`removeDemo: ${removeDemo}`))
-      p(yellow(`removeDemoPart: ${removeDemoPart}`))
-      // process.exit(1)
+      startSpinner(` Removing fancy demo ${removeDemoPart}`)
       try {
         const IGNITE = "node " + filesystem.path(__dirname, "..", "..", "bin", "ignite")
         const CMD = removeDemo === true ? "remove-demo" : "remove-demo-markup"
@@ -843,7 +847,7 @@ module.exports = {
         log(e)
         p(yellow(`Unable to remove demo ${removeDemoPart}.`))
       }
-      stopSpinner(`Removing fancy demo ${removeDemoPart}`, "🛠️")
+      stopSpinner(` Removing fancy demo ${removeDemoPart}`, "🛠️")
       // #endregion
 
       // #region Expo Router edits
