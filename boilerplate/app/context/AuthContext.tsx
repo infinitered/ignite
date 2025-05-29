292724b3110fee3052d07b1d2abef6fc882d346f
Mark Rickert <139261+markrickert@users.noreply.github.com>
2025-05-29 16:08:04 -0600

    chore: syntax update suggestion
    Co-authored-by: Jamon Holmgren <code@jamon.dev>

--- boilerplate/app/context/AuthContext.tsx
+++ boilerplate/app/context/AuthContext.tsx
@@ -46,9 +46,7 @@ export const AuthProvider: FC<PropsWithChildren<AuthProviderProps>> = ({ childre
 
 export const useAuth = () => {
   const context = useContext(AuthContext)
-  if (!context) {
-    throw new Error("useAuth must be used within an AuthProvider")
-  }
+  if (!context) throw new Error("useAuth must be used within an AuthProvider")
   return context
 }
 
