de865eff6d78230552675502b499c2e6ca220280
Tyler Williams <tyler@coolsoftware.dev>
2025-09-26 14:57:29 -0400

    fix: mock nitro modules

--- boilerplate/test/setup.ts
+++ boilerplate/test/setup.ts
@@ -51,6 +51,15 @@ jest.mock("../app/i18n/index.ts", () => ({
   },
 }))
 
+jest.mock("react-native-nitro-modules", () => ({
+  createHybridObject: () => ({
+    init: () => {},
+    add: () => {},
+    createHybridNavigationBar: () => {},
+    createHybridStatusBar: () => ({}),
+  }),
+}))
+
 declare const tron // eslint-disable-line @typescript-eslint/no-unused-vars
 
 declare global {
